098d5060dc3717101ddb527f430f6f26ef30699b
Alvfan86 <Alvfan86@gmail.com>
2020-01-15 15:16:43 +0100

    Move VueMq import to main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ import router from "./router";
 import store from "./store";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
+import VueMq from "vue-mq";
+
+Vue.use(VueMq);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount("#app");
