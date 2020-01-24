a17edb0b9d0d4b78448adfff9c1307ca85225395
Alvfan86 <Alvfan86@gmail.com>
2020-01-24 11:22:04 +0100

    Validate hour input

--- src/components/HourInput.vue
+++ src/components/HourInput.vue
@@ -1,57 +1,99 @@
 <template>
   <form novalidate>
+    <button @click="onSevenFiveClick" v-if="showHelperButtons">7,5</button>
     <input
       :class="{ error: hasError }"
-      :type="type"
-      v-model="value"
+      type="text"
+      :value="value"
+      @input="debouncedValueSet"
       @touchstart="onTouchStart"
+      @blur="onBlur"
       novalidate
+      inputmode="decimal"
+      ref="inputRef"
     />
   </form>
 </template>
 
 <script>
-import debounce from "lodash/debounce";
+import { defer } from "lodash";
 
 export default {
   props: ["timeEntrie"],
   data() {
     return {
-      type: "text",
       hasError: false,
-      erroneousValue: 0,
+      rawVaue: 0,
+      showHelperButtons: false,
+      enableBlur: true,
+      valueSetTimeout: 0,
     };
   },
 
   computed: {
-    value: {
-      get() {
-        if (this.hasError) {
-          return this.erroneousValue;
-        }
-        const { id, date } = this.timeEntrie;
-        const entrie = this.$store.getters.getTimeEntrie(id, date);
-        return entrie ? entrie.value.toString().replace(".", ",") : 0;
-      },
-      set: debounce(function(str) {
-        this.erroneousValue = str;
-        str = str.replace(/,/, ".");
-        if (str.match(/[^0-9.]/g)) {
-          this.hasError = true;
-          return;
-        }
-        this.hasError = false;
-        const value = Number(str);
-        const timeEntrie = { ...this.timeEntrie, value };
-        this.$store.dispatch("UPDATE_TIME_ENTRIE", timeEntrie);
-      }, 2000),
+    inputRef() {
+      return this.$refs.inputRef;
+    },
+
+    value() {
+      if (this.hasError) {
+        return this.rawValue;
+      }
+      const { id, date } = this.timeEntrie;
+      const entrie = this.$store.getters.getTimeEntrie(id, date);
+      return entrie ? entrie.value.toString().replace(".", ",") : 0;
     },
   },
 
   methods: {
+    debouncedValueSet({ target: { value: str } }) {
+      if (this.valueSetTimeout) {
+        clearTimeout(this.valueSetTimeout);
+      }
+      this.valueSetTimeout = setTimeout(() => {
+        this.valueSet(str);
+      }, 2000);
+    },
+
+    valueSet(str) {
+      this.rawValue = str;
+      str = str.replace(/,/, ".");
+      if (!isValidInput(str)) {
+        this.hasError = true;
+        return;
+      }
+      this.hasError = false;
+
+      const value = Number(str);
+      const timeEntrie = { ...this.timeEntrie, value };
+      this.$store.dispatch("UPDATE_TIME_ENTRIE", timeEntrie);
+    },
+
+    isValidInput(str) {
+      return str.match(/[^0-9.,]/g) || str.split(".").length > 2;
+    },
+
     onTouchStart() {
-      this.type = "number";
-      setTimeout(() => (this.type = "text"), 200);
+      setTimeout(() => {
+        this.showHelperButtons = true;
+        this.inputRef.focus();
+      }, 200);
+    },
+
+    onBlur() {
+      defer(() => {
+        if (this.enableBlur) {
+          this.showHelperButtons = false;
+        }
+        this.enableBlur = true;
+      });
+    },
+
+    onSevenFiveClick() {
+      const timeEntrie = { ...this.timeEntrie, value: 7.5 };
+      this.$store.dispatch("UPDATE_TIME_ENTRIE", timeEntrie);
+      this.enableBlur = false;
+      this.inputRef.focus();
     },
   },
 };
@@ -65,9 +107,25 @@ input {
   padding: 0.4rem;
   font-size: 0.8rem;
   border-radius: 0;
+  border: 1px solid black;
+}
+
+input:focus {
+  outline: none;
 }
 
 .error {
   border-color: red;
+}
+
+button {
+  background-color: #e8b925;
+  border: none;
+  padding: 0.3rem 0.4rem;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 0.8rem;
+  margin-right: 0.4rem;
 }
 </style>
