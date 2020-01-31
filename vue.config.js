6c7ff64f98f53cf93fde0f57197b818a99c72d13
Alvfan86 <Alvfan86@gmail.com>
2020-01-31 11:51:37 +0100

    Add Apple support to pwa settings

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   lintOnSave: false,
   pwa: {
+    name: "Alvtime",
+    themeColor: "#061838",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
       swSrc: "./src/sw.js",
