0f8d85e8553a1edb03c802ef342aa676c7e16027
Pål Bøckmann <plbockmann@gmail.com>
2020-02-06 12:19:24 +0100

    Fixed timeentries api

--- AlvTimeWebApi2/Dto/TimeEntriesResponseDto.cs
+++ AlvTimeWebApi2/Dto/TimeEntriesResponseDto.cs
@@ -4,6 +4,7 @@ namespace AlvTimeApi.Controllers.Tasks
 {
     public class TimeEntriesResponseDto
     {
+        public int Id { get; set; }
         public DateTime Date { get; set; }
         public decimal Value { get; set; }
         public int TaskId { get; set; }
