5a492ecb7c119c3aeff19ca4932c1f496afed104
Pål Bøckmann <plbockmann@gmail.com>
2020-02-09 12:51:46 +0100

    Trimmed usernames in InsertData

--- AlvTimeWebApi2/Database/InsertData.sql
+++ AlvTimeWebApi2/Database/InsertData.sql
@@ -3,35 +3,35 @@ GO
 
 insert into [dbo].[Customer]
 values 
-('SuperMat       '),                                                                                        
-('Rutsjebaner AS '),                                                                                        
-('Film og TV     '),                                                                                        
-('Datafabrikken  '),                                                                                                                                                                               
-('Alv            ')       
+('SuperMat'),                                                                                        
+('Rutsjebaner AS'),                                                                                        
+('Film og TV'),                                                                                        
+('Datafabrikken'),                                                                                                                                                                               
+('Alv')       
 
 insert into [dbo].[Project]
 values 
-('MatAppen        ' ,1),
-('LeverMatTilMeg  ' ,1),
-('Sklier          ' ,2),
-('Luksussmellen   ' ,3),
-('Paradise Motell ' ,3),
-('Disks n Chips   ' ,4),
-('Hurramegrundt   ' ,2),
-('Alv             ' ,5)
+('MatAppen' ,1),
+('LeverMatTilMeg' ,1),
+('Sklier' ,2),
+('Luksussmellen' ,3),
+('Paradise Motell' ,3),
+('Disks n Chips' ,4),
+('Hurramegrundt' ,2),
+('Alv' ,5)
 
 insert into [dbo].[User]
 values
-('Ansatt En  ', 'ansatten@alv.no'),
-('Ansatt To  ', 'ansatto@alv.no'),
-('Ansatt Tre  ', 'ansattre@alv.no'),
-('Ansatt Fire  ', 'ansattfire@alv.no'),
-('Ansatt Fem  ', 'ansattfem@alv.no'),
-('Ansatt Seks  ', 'ansattseks@alv.no'),
-('Ansatt Syv  ', 'ansattsyv@alv.no'),
-('Ansatt Atte  ', 'ansattatte@alv.no'),
-('Ansatt Ni  ', 'ansattni@alv.no'),
-('Ansatt Ti  ', 'ansatti@alv.no')
+('Ansatt En', 'ansatten@alv.no'),
+('Ansatt To', 'ansatto@alv.no'),
+('Ansatt Tre', 'ansattre@alv.no'),
+('Ansatt Fire', 'ansattfire@alv.no'),
+('Ansatt Fem', 'ansattfem@alv.no'),
+('Ansatt Seks', 'ansattseks@alv.no'),
+('Ansatt Syv', 'ansattsyv@alv.no'),
+('Ansatt Atte', 'ansattatte@alv.no'),
+('Ansatt Ni', 'ansattni@alv.no'),
+('Ansatt Ti', 'ansatti@alv.no')
 
 insert into [dbo].[TaskFavorites]
 select id as UserId, 12 as TaskId
@@ -59,23 +59,23 @@ from [dbo].[User]
 
 insert into [dbo].[Task]
 values
-('Testleder              ','',	2,	1000.00, 0, 0),
-('Funksjonell arkitekt   ','',	2,	1000.00, 0, 0),
-('Teamleder              ','',	6,	1000.00, 0, 0),
-('Prosjektstotte         ','',	6,	1000.00, 0, 0),
-('Testleder              ','',	3,	1000.00, 0, 0),
-('Testradgiver           ','',	3,	1000.00, 0, 0),
-('Cost controller        ','',	8,	1000.00, 0, 0),
-('Utvikler               ','',	7,	1000.00, 0, 0),
-('Seniorutvikler         ','',	1,	1000.00, 0, 0),
-('Juniorutvikler         ','',	1,	1000.00, 0, 0),
-('Sikkerhetstester       ','',	2,	1000.00, 0, 0),
-('Interntid              ','',	8,	0.00, 0, 0),
-('Ferie                  ','',	8,	0.00, 0, 0),
-('Syk                    ','',	8,	0.00, 0, 0),
-('Sykt barn              ','',	8,	0.00, 0, 0),
-('Youtube serie          ','',	8,	0.00, 0, 0),
-('AlvTimeUtvikling       ','',	8,	0.00, 0, 0)
+('Testleder','',	2,	1000.00, 0, 0),
+('Funksjonell arkitekt','',	2,	1000.00, 0, 0),
+('Teamleder','',	6,	1000.00, 0, 0),
+('Prosjektstotte','',	6,	1000.00, 0, 0),
+('Testleder','',	3,	1000.00, 0, 0),
+('Testradgiver','',	3,	1000.00, 0, 0),
+('Cost controller','',	8,	1000.00, 0, 0),
+('Utvikler','',	7,	1000.00, 0, 0),
+('Seniorutvikler','',	1,	1000.00, 0, 0),
+('Juniorutvikler','',	1,	1000.00, 0, 0),
+('Sikkerhetstester','',	2,	1000.00, 0, 0),
+('Interntid','',	8,	0.00, 0, 0),
+('Ferie','',	8,	0.00, 0, 0),
+('Syk','',	8,	0.00, 0, 0),
+('Sykt barn','',	8,	0.00, 0, 0),
+('Youtube serie','',	8,	0.00, 0, 0),
+('AlvTimeUtvikling','',	8,	0.00, 0, 0)
 
 insert into [dbo].[Hours]
 values
