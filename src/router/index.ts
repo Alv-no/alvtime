e5f6c2066c16f9df0f7a9e698bea3a73e4fd1db3
Alvfan86 <Alvfan86@gmail.com>
2020-02-19 22:01:07 +0100

    Stop lazy loading the only route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "*",
     name: "home",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/Home.vue")
-  }
+    component: Home,
+  },
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
 });
 
 export default router;
