6901b89f21910033ba2c2f467e2286b531213cd3
Pål Bøckmann <plbockmann@gmail.com>
2020-03-04 09:53:52 +0100

    Sort usings

--- AlvTimeWebApi2/DataBaseModels/AlvTimeDBContext.cs
+++ AlvTimeWebApi2/DataBaseModels/AlvTimeDBContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace AlvTimeWebApi2.DataBaseModels
 {
@@ -29,7 +27,6 @@ namespace AlvTimeWebApi2.DataBaseModels
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=AlvTimeDB;Trusted_Connection=True;");
             }
         }
