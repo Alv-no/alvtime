2ee16dc4b0495ce6d21d274dcc2bd66e5b5df7f6
Pål Bøckmann <plbockmann@gmail.com>
2020-03-23 12:43:34 +0100

    Added failing test

--- Tests/UnitTests/ExampleUT.cs
+++ Tests/UnitTests/ExampleUT.cs
@@ -6,13 +6,23 @@ namespace Tests
     public class ExampleUT
     {
         [Fact]
-        public void Test1()
+        public void Test1_Passes()
         {
             string s1 = "hel";
             string s2 = "lo";
             string s3 = s1 + s2;
 
             Assert.Equal("hello", s3);
+        }
+
+        [Fact]
+        public void Test2_Fails()
+        {
+            string s1 = "hel";
+            string s2 = "lo";
+            string s3 = s1 + s2;
+
+            Assert.Equal("helo", s3);
         }
     }
 }
