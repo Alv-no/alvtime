0046f93135447f2feebd7f1cc7d855e69421dcde
Truls Aagaard <truls@aagaard.io>
2020-03-23 10:20:17 +0100

    Implemented viritualization in WeekSwiper

--- src/components/WeekSwiper.vue
+++ src/components/WeekSwiper.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <WeekHeader @backClick="onPrev" @forwardClick="onNext" />
-    <swiper
-      @slideChangeTransitionEnd="onSlideChangeTransitionEnd"
-      ref="mySwiper"
-      :options="swiperOption"
-    >
-      <swiperSlide v-for="(week, index) in weeks" :key="index">
-        <TimeEntrieWeekList :week="week" />
-      </swiperSlide>
-    </swiper>
+    <div ref="mySwiper" class="swiper-container">
+      <div class="swiper-wrapper">
+        <div
+          class="swiper-slide"
+          v-for="(week, index) in virtualData.slides"
+          :key="index"
+          :style="{ left: `${virtualData.offset}px` }"
+        >
+          <TimeEntrieWeekList :week="week" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,37 +20,54 @@
 import Vue from "vue";
 import moment from "moment";
 import config from "@/config";
-import { swiper, swiperSlide } from "vue-awesome-swiper";
 import TimeEntrieWeekList from "./TimeEntrieWeekList.vue";
 import WeekHeader from "./WeekHeader.vue";
 import isInIframe from "@/mixins/isInIframe";
 import { createWeek } from "@/mixins/date";
 
+import Swiper from "swiper";
+
 export default Vue.extend({
   components: {
-    swiper,
-    swiperSlide,
     TimeEntrieWeekList,
     WeekHeader,
   },
 
   data() {
     return {
-      swiperOption: {
-        initialSlide: 6,
-        shortSwipes: false,
-        simulateTouch: false,
-        noSwipingSelector: "input, button",
-        longSwipesRatio: 0.15,
-        longSwipesMs: 100,
-        keyboard: {
-          enabled: true,
-          onlyInViewport: false,
+      swiperOption: {},
+      // @ts-ignore
+      weeks: this.createManyWeeks(),
+      virtualData: [],
+    };
+  },
+
+  mounted() {
+    const self = this;
+    const swiper = new Swiper(".swiper-container", {
+      initialSlide: 142,
+      shortSwipes: false,
+      simulateTouch: false,
+      noSwipingSelector: "input, button",
+      longSwipesRatio: 0.15,
+      longSwipesMs: 100,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+      on: {
+        // @ts-ignore
+        slideChangeTransitionEnd: self.onSlideChangeTransitionEnd,
+      },
+      virtual: {
+        // @ts-ignore
+        slides: self.weeks,
+        renderExternal(data: any) {
+          // @ts-ignore
+          self.virtualData = data;
         },
       },
-      weeks: createThreeMonths(this.$store.state.activeDate),
-      swiperObject: null,
-    };
+    });
   },
 
   created() {
@@ -69,52 +89,24 @@ export default Vue.extend({
       this.swiper.slidePrev();
     },
 
-    appendSlides() {
-      // @ts-ignore
-      const firstDayInLastWeek = this.weeks[this.weeks.length - 1][0];
-      const firstDayInFirstNewWeek = firstDayInLastWeek.clone().add(1, "week");
-      const newWeeks = createFourWeeksFromDate(firstDayInFirstNewWeek);
-      // @ts-ignore
-      this.weeks = [...this.weeks.slice(4), ...newWeeks];
-      // @ts-ignore
-      this.swiper.slideTo(this.swiper.realIndex - 4, 0);
-      // @ts-ignore
-      this.$store.dispatch("FETCH_TIME_ENTRIES", this.dateRange);
-    },
-
-    prependSlides() {
-      // @ts-ignore
-      const firstDayInFirstWeek = this.weeks[0][0];
-      const firstDayInFirstNewWeek = firstDayInFirstWeek
-        .clone()
-        .add(-4, "week");
-      const newWeeks = createFourWeeksFromDate(firstDayInFirstNewWeek);
-      // @ts-ignore
-      this.weeks = [...newWeeks, ...this.weeks.slice(0, 8)];
-      // @ts-ignore
-      this.swiper.slideTo(this.swiper.realIndex + 4, 0);
-      // @ts-ignore
-      this.$store.dispatch("FETCH_TIME_ENTRIES", this.dateRange);
-    },
-
     onSlideChangeTransitionEnd() {
-      // @ts-ignore
-      if (this.swiper.activeIndex > 8) {
-        // @ts-ignore
-        this.appendSlides();
-      }
-      // @ts-ignore
-      if (this.swiper.activeIndex < 4) {
-        // @ts-ignore
-        this.prependSlides();
-      }
-
       const dayOfWeek = this.$store.state.activeDate.weekday();
       this.$store.commit(
         "UPDATE_ACTVIE_DATE",
         // @ts-ignore
         this.weeks[this.swiper.activeIndex][dayOfWeek]
       );
+    },
+
+    createManyWeeks() {
+      const date = this.$store.state.activeDate;
+      const future = Array.apply(null, Array(142)).map((n, i) => i);
+      const past = Array.apply(null, Array(142))
+        .map((n, i) => (i + 1) * -1)
+        .reverse();
+      return [...past, ...future]
+        .map(n => date.clone().add(n, "week"))
+        .map(createWeek);
     },
   },
 
@@ -136,20 +128,4 @@ export default Vue.extend({
     },
   },
 });
-
-function createFourWeeksFromDate(date: moment.Moment) {
-  return Array.apply(null, Array(4))
-    .map((n, i) => date.clone().add(i, "week"))
-    .map(createWeek);
-}
-
-function createThreeMonths(date: moment.Moment) {
-  const future = Array.apply(null, Array(6)).map((n, i) => i);
-  const past = Array.apply(null, Array(6))
-    .map((n, i) => (i + 1) * -1)
-    .reverse();
-  return [...past, ...future]
-    .map(n => date.clone().add(n, "week"))
-    .map(createWeek);
-}
 </script>
