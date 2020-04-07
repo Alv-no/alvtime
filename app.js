3d0b81ffbb1940b1d8b9d9c4b5f8d355a6a839da
Truls Aagaard <truls@aagaard.io>
2020-04-07 19:57:25 +0200

    Init commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Require the Bolt package (github.com/slackapi/bolt)
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+
+
+// All the room in the world for your code
+
+
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
