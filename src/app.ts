9f8219b44e291a5c897749040735102ae386a504
Truls Aagaard <truls@aagaard.io>
2020-04-07 21:39:30 +0200

    Add typescript, eslint, and jest with friends
    Closes #3

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 // Require the Bolt package (github.com/slackapi/bolt)
-const { App } = require("@slack/bolt");
+import { App } from "@slack/bolt";
+import dotenv from "dotenv";
 
-const app = new App({
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: ".env" });
+}
+
+const app: App = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
 
-
 // All the room in the world for your code
-
 
 
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
