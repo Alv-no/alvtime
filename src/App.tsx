669225ab5d29353c2271c0f0db0543c83f5f6a36
Truls Aagaard <truls@aagaard.io>
2020-04-24 13:52:10 +0200

    Add azureAd service
    Closes #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { getAccount, login, logout } from "./services/azureAd";
+import { Account } from "@azure/msal-common";
+
+const Json = ({ data }: any) => <pre>{JSON.stringify(data, null, 4)}</pre>;
 
 function App() {
+  const [account, setAccount] = useState({} as Account);
+  const [error, setError] = useState("");
+
+  const onSignInClick = async () => {
+    const loginResponse = await login().catch((error) => {
+      setError(error.message);
+    });
+    if (loginResponse && loginResponse.account) {
+      setAccount(loginResponse.account);
+    }
+  };
+
+  const onGetAccountClick = () => {
+    const account = getAccount();
+    setAccount(account);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+        <h1>
+          Welcome to the Microsoft Authentication Library For Javascript - React
+          Quickstart
+        </h1>
+        {!account.name ? (
+          <>
+            <button onClick={onSignInClick}>Sign In</button>
+          </>
+        ) : (
+          <>
+            <button onClick={logout}>Sign Out</button>
+            <button onClick={onGetAccountClick}>View account</button>
+          </>
+        )}
+        {error && <p className="error">Error: {error}</p>}
+      </section>
+      <section className="data">
+        {account.name && (
+          <div className="data-account">
+            <h2>Session Account Data</h2>
+            <Json data={account} />
+          </div>
+        )}
+      </section>
     </div>
   );
 }
