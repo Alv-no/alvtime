33bce671b3ea99133d6587460a84ade25f80e16d
Truls Aagaard <truls@aagaard.io>
2020-04-27 20:47:52 +0200

    Convert from using Bolt to SDK

--- src/app.ts
+++ src/app.ts
@@ -1,203 +1,198 @@
-// Require the Bolt package (github.com/slackapi/bolt)
-import { App } from "@slack/bolt";
+import { createEventAdapter } from "@slack/events-api";
+import { createMessageAdapter } from "@slack/interactive-messages";
+import { WebClient } from "@slack/web-api";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
-
+import express from "express";
 
 if (process.env.NODE_ENV !== "production") {
   dotenv.config({ path: ".env" });
 }
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
-});
+const port = process.env.PORT || 3000;
+const app = express();
+const token = process.env.SLACK_BOT_TOKEN;
+const webClient = new WebClient(token);
 
-app.event('app_home_opened', async ({ event, context }) => {
-  try {
-    /* view.publish is the method that your app uses to push a view to the Home tab */
-    const result = await app.client.views.publish({
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+const slackInteractions = createMessageAdapter(
+  process.env.SLACK_SIGNING_SECRET
+);
 
-      /* retrieves your xoxb token from context */
-      token: context.botToken,
+app.use("/slack/events", slackEvents.expressMiddleware());
+app.use("/slack/actions", slackInteractions.expressMiddleware());
 
-      /* the user that opened your app's app home */
-      user_id: event.user,
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-      /* the view payload that appears in the app home*/
-      view: {
-        type: 'home',
-        callback_id: 'home_view',
-
-        /* body of the view */
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
-                "action_id": 'button_abc',
-              }
-            ]
-          }
-        ]
-      }
-    });
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
-
-// Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say({
-    blocks: [
+const messageJsonBlock = {
+  blocks: [
     {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": `Hey there <@${message.user}>!`
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "Hello, thanks for calling me. Would you like to launch a modal?",
       },
-      "accessory": {
-        "type": "button",
-        "text": {
-          "type": "plain_text",
-          "text": "Click Me"
+      accessory: {
+        type: "button",
+        action_id: "open_modal_button", // We need to add this
+        text: {
+          type: "plain_text",
+          text: "Launch",
+          emoji: true,
         },
-        "action_id": "button_click"
-      }
-    }
-    ]
-  });
-});
+        value: "launch_button_click",
+      },
+    },
+  ],
+};
 
-app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
-});
-
-// Listen for a button invocation with action_id `button_abc`
-// You must set up a Request URL under Interactive Components on your app configuration page
-app.action('button_abc', async ({ ack, body, context }) => {
-  // Acknowledge the button request
-  ack();
-
-  try {
-    // Update the message
-    const result = await app.client.chat.update({
-      token: context.botToken,
-      // ts of message to update
-      ts: body.message.ts,
-      // Channel of message
-      channel: body.channel.id,
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: '*The button was clicked!*'
-          }
-        }
-      ],
-      text: 'Message from Test App'
-    });
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
-
-// Listen for a slash command invocation
-app.command('/alvtime', async ({ ack, body, context }) => {
-  // Acknowledge the command request
-  await ack();
-
-  try {
-    const result = await app.client.views.open({
-      token: context.botToken,
-      // Pass a valid trigger_id within 3 seconds of receiving it
-      trigger_id: body.trigger_id,
-      // View payload
-      view: {
-        type: 'modal',
-        // View identifier
-        callback_id: 'view_1',
-        title: {
-          type: 'plain_text',
-          text: 'Modal title'
+const modalJsonBlock = {
+  type: "modal",
+  callback_id: "cute_animal_modal_submit", // We need to add this
+  title: {
+    type: "plain_text",
+    text: "My App",
+    emoji: true,
+  },
+  submit: {
+    type: "plain_text",
+    text: "Done",
+    emoji: true,
+  },
+  close: {
+    type: "plain_text",
+    text: "Cancel",
+    emoji: true,
+  },
+  blocks: [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "Thanks for openeing this modal!",
+      },
+    },
+    {
+      type: "input",
+      block_id: "cute_animal_selection_block", // put this here to identify the selection block
+      element: {
+        type: "static_select",
+        action_id: "cute_animal_selection_element", // put this here to identify the selection element
+        placeholder: {
+          type: "plain_text",
+          text: "Select a cute animal",
+          emoji: true,
         },
-        blocks: [
+        options: [
           {
-            type: 'section',
             text: {
-              type: 'mrkdwn',
-              text: 'Welcome to a modal with _blocks_'
+              type: "plain_text",
+              text: "Puppy",
+              emoji: true,
             },
-            accessory: {
-              type: 'button',
-              text: {
-                type: 'plain_text',
-                text: 'Click me!'
-              },
-              action_id: 'button_abc'
-            }
+            value: "puppy",
           },
           {
-            type: 'input',
-            block_id: 'input_c',
-            label: {
-              type: 'plain_text',
-              text: 'What are your hopes and dreams?'
+            text: {
+              type: "plain_text",
+              text: "Kitten",
+              emoji: true,
             },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'dreamy_input',
-              multiline: true
-            }
-          }
+            value: "kitten",
+          },
+          {
+            text: {
+              type: "plain_text",
+              text: "Bunny",
+              emoji: true,
+            },
+            value: "bunny",
+          },
         ],
-        submit: {
-          type: 'plain_text',
-          text: 'Submit'
-        }
-      }
-    });
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
+      },
+      label: {
+        type: "plain_text",
+        text: "Choose a cute pet:",
+        emoji: true,
+      },
+    },
+    {
+      type: "input",
+      block_id: "cute_animal_name_block", // put this here to identify the input.
+      element: {
+        type: "plain_text_input",
+        action_id: "cute_animal_name_element", // put this here to identify the selection element
+      },
+      label: {
+        type: "plain_text",
+        text: "Give it a cute name:",
+        emoji: true,
+      },
+    },
+  ],
+};
+
+slackEvents.on("app_mention", async (event: any) => {
+  try {
+    const mentionResponseBlock = {
+      ...messageJsonBlock,
+      ...{ channel: event.channel },
+    };
+    console.log(mentionResponseBlock);
+    const res = await webClient.chat.postMessage(mentionResponseBlock);
+    console.log("Message sent: ", res.ts);
+  } catch (e) {
+    console.log(JSON.stringify(e));
   }
 });
 
+slackInteractions.action({ actionId: "open_modal_button" }, async (payload) => {
+  try {
+    await webClient.views.open({
+      trigger_id: payload.trigger_id,
+      view: modalJsonBlock,
+    });
+  } catch (e) {
+    console.log(JSON.stringify(e));
+  }
 
-(async () => {
-  const port = process.env.PORT || 3000;
-  // Start your app
-  await app.start(port);
+  // The return value is used to update the message where the action occurred immediately.
+  // Use this to items like buttons and menus that you only want a user to interact with once.
+  return {
+    text: "Processing...",
+  };
+});
 
-  console.log(`⚡️ Bolt app is running on port: ${port}`);
-})();
+slackInteractions.viewSubmission(
+  "cute_animal_modal_submit",
+  async (payload) => {
+    const blockData = payload.view.state;
+
+    const cuteAnimalSelection =
+      blockData.values.cute_animal_selection_block.cute_animal_selection_element
+        .selected_option.value;
+    const nameInput =
+      blockData.values.cute_animal_name_block.cute_animal_name_element.value;
+
+    console.log(cuteAnimalSelection, nameInput);
+
+    if (nameInput.length < 2) {
+      return {
+        response_action: "errors",
+        errors: {
+          cute_animal_name_block:
+            "Cute animal names must have more than one letter.",
+        },
+      };
+    }
+    return {
+      response_action: "clear",
+    };
+  }
+);
+
+// Starts server
+app.listen(port, function () {
+  console.log("Bot is listening on port " + port);
+});
