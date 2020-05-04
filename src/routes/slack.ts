19fa025a2753c5b73035e19baaf28392c1228f95
Truls Aagaard <truls@aagaard.io>
2020-05-04 13:38:05 +0200

    Add /command route

--- src/routes/slack.ts
+++ src/routes/slack.ts
@@ -18,4 +18,69 @@ slackRouter.use("/actions", slackInteractions.expressMiddleware());
 slackRouter.use(bodyParser.urlencoded({ extended: true }));
 slackRouter.use(bodyParser.json());
 
+interface CommandBody {
+  token: string;
+  team_id: string;
+  team_domain: string;
+  channel_id: string;
+  channel_name: string;
+  user_id: string;
+  user_name: string;
+  command: string;
+  text: string;
+  response_url: string;
+  trigger_id: string;
+}
+
+slackRouter.use("/command", (req, res, next) => {
+  console.log(req.body);
+  next();
+});
+
+slackRouter.use("/command", (req, res, next) => {
+  const { text, channel_id, user_name } = req.body;
+  if (text === "test") {
+    const loginMessage = createLoginMessage(user_name, channel_id);
+    slackWebClient.chat.postMessage(loginMessage);
+    res.send("");
+  }
+  next();
+});
+
+function createLoginMessage(name: string, channelId: string) {
+  return {
+    text: "",
+    blocks: [
+      { type: "section", text: { type: "mrkdwn", text: `Hei ${name} :wave:` } },
+      {
+        type: "section",
+        text: { type: "mrkdwn", text: "Du er ikke logget inn." },
+        accessory: {
+          type: "button",
+          action_id: "open_login_in_browser",
+          text: { type: "plain_text", text: "Login med Azure Ad", emoji: true },
+          url: env.HOST + "/oauth2/azuread",
+          value: "login_button_clicked",
+        },
+      },
+    ],
+    channel: channelId,
+  };
+}
+
+slackRouter.post("/command", (req, res) => {
+  const body = req.body as CommandBody;
+  slackWebClient.chat.postMessage({ text: "HEI", channel: body.channel_id });
+  res.send("");
+});
+
+slackInteractions.action(
+  { actionId: "open_login_in_browser" },
+  async function () {
+    return {
+      text: "Åpner login nettside...",
+    };
+  }
+);
+
 export default slackRouter;
