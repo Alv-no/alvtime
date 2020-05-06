374cb91e9a996508217943d55f659892c193e7ad
Truls Aagaard <truls@aagaard.io>
2020-05-06 12:21:08 +0200

    Create styled Input component

--- src/components/GenerateToken.vue
+++ src/components/GenerateToken.vue
@@ -7,7 +7,7 @@
       mot Azure Ad.
     </div>
     <div class="container">
-      <input v-model="friendlyName" placeholder="Hva skal tokenet brukes til" />
+      <Input v-model="friendlyName" placeholder="Hva skal tokenet brukes til" />
       <YellowButton
         icon-id="add_circle_outline"
         :text="buttonText"
@@ -37,6 +37,7 @@
 import Vue from "vue";
 import YellowButton from "./YellowButton.vue";
 import Tooltip from "./Tooltip.vue";
+import Input from "./Input.vue";
 import { adAuthenticatedFetch } from "@/services/auth";
 import config from "@/config";
 
@@ -44,6 +45,7 @@ export default Vue.extend({
   components: {
     YellowButton,
     Tooltip,
+    Input,
   },
 
   data() {
@@ -145,29 +147,6 @@ export default Vue.extend({
 .outer-container {
   display: grid;
   justify-content: center;
-}
-
-input {
-  appearance: none;
-  -moz-appearance: textfield;
-  width: 100%;
-  padding: 0.4rem;
-  font-size: 0.8rem;
-  border-radius: 5px;
-  border: 1px solid #e0e0e0;
-  background-color: #f7f7f7;
-  font-size: 0.8rem;
-  line-height: 0.7rem;
-  height: 2rem;
-}
-
-input:focus {
-  outline: none;
-}
-
-input:hover {
-  border-color: #008dcf;
-  transition: border-color 500ms ease-out;
 }
 
 .icon_button {
---
+++ src/components/Input.vue
@@ -0,0 +1,61 @@
+<template>
+  <input
+    ref="inputRef"
+    :type="type"
+    :value="value"
+    :placeholder="placeholder"
+    @input="update"
+  />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+  },
+
+  methods: {
+    update() {
+      // @ts-ignore
+      this.$emit("input", this.$refs.inputRef.value);
+    },
+  },
+});
+</script>
+
+<style scoped>
+input {
+  appearance: none;
+  -moz-appearance: textfield;
+  width: 100%;
+  padding: 0.4rem;
+  font-size: 0.8rem;
+  border-radius: 5px;
+  border: 1px solid #e0e0e0;
+  background-color: #f7f7f7;
+  font-size: 0.8rem;
+  line-height: 0.7rem;
+  height: 2rem;
+}
+
+input:focus {
+  outline: none;
+}
+
+input:hover {
+  border-color: #008dcf;
+  transition: border-color 500ms ease-out;
+}
+</style>
