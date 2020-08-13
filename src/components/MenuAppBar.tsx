ff57c088a5da9a066246a9b0e737f9c1eb278e82
Rein Oterholm <rein@otter.productions>
2020-08-13 19:25:16 +0200

    added drawer

--- src/components/MenuAppBar.tsx
+++ src/components/MenuAppBar.tsx
@@ -2,9 +2,20 @@ import AppBar from "@material-ui/core/AppBar";
 import IconButton from "@material-ui/core/IconButton";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
+import Drawer from "@material-ui/core/Drawer";
 import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
 import React from "react";
+
+import clsx from 'clsx';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,11 +28,81 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       flexGrow: 1,
     },
+    list: {
+    width: 250
+  },
+  fullList: {
+    width: "auto"
+  }
   })
 );
 
 export default function MenuAppBar() {
   const classes = useStyles();
+  // const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  const drawer_side = "right";
+
+  const list_items = [
+    {
+      description: "Timeføring",
+      route: "/hours"
+    },{
+      description: "Admin",
+      route: "/admin"
+    }, {
+      description: "Project",
+      route: "/project"
+    },{
+      description: "Rapportering",
+      route: "/reports"
+    }
+  ]
+
+
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor:string, open:boolean) => (event:any) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState({ ...state, [anchor]: open });
+  };
+
+
+  const list = (anchor:string) => (
+    <div
+      role="presentation"
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom"
+      })}
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
   return (
     <div className={classes.root}>
@@ -31,14 +112,26 @@ export default function MenuAppBar() {
             Alvtime Admin
           </Typography>
           <div>
+
             <IconButton
               edge="start"
               className={classes.menuButton}
               color="inherit"
               aria-label="menu"
+              onClick={toggleDrawer(drawer_side, true)}
             >
               <MenuIcon />
+
             </IconButton>
+
+            <React.Fragment key={drawer_side}>
+             <Drawer anchor={drawer_side} open={state[drawer_side]} onClose={toggleDrawer(drawer_side, false)}>
+               {list(drawer_side)}
+             </Drawer>
+           </React.Fragment>
+
+
+
           </div>
         </Toolbar>
       </AppBar>
