1b7e8f0076e46b6ba83c62d34fd974462d22a579
Rein Oterholm <rein@otter.productions>
2020-08-13 22:51:02 +0200

    added router for navigation and windows scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import theme from "./theme";
 
@@ -10,8 +15,12 @@ import Login from "./components/Login";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <MenuAppBar />
-      <Login />
+    <Router>
+    <MenuAppBar/>
+      <Switch>
+        <Route exact path="/" component={Login} />
+      </Switch>
+    </Router>
     </ThemeProvider>
   );
 }
