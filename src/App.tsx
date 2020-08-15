e6f61c2622afdbd616a7c3fa2f8b3b35e3567831
Truls Aagaard <truls@aagaard.io>
2020-08-15 10:30:50 +0200

    Ran prettier on all code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import theme from "./theme";
 
@@ -11,16 +7,15 @@ import theme from "./theme";
 import MenuAppBar from "./components/MenuAppBar";
 import Login from "./components/Login";
 
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <Router>
-    <MenuAppBar/>
-      <Switch>
-        <Route exact path="/" component={Login} />
-      </Switch>
-    </Router>
+      <Router>
+        <MenuAppBar />
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
--- src/services/createMsalApp.js
+++ src/services/createMsalApp.js
@@ -1,3 +1,3 @@
 // This is in its own js file because msal-browser is not properly typed yet
 import { PublicClientApplication } from "@azure/msal-browser";
-export default options => new PublicClientApplication(options);
+export default (options) => new PublicClientApplication(options);
