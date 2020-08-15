dbffc143cc50d7f81972f8f97e1ca6d322fbd793
Truls Aagaard <truls@aagaard.io>
2020-08-15 17:14:41 +0200

    Enable use of TEST_ACCESS_TOKEN

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,5 @@ export default Object.freeze({
   CLIENT_ID: "373a7768-6d2a-4b20-b671-bc1089104a3a",
   ACCESS_SCOPE:
     "https://d8aedd.alvtime-ap-alvtime-api-dev-f9b4e4.westeurope.cloudapp.azure.com/access_as_user",
+  TEST_ACCESS_TOKEN: "5801gj90-jf39-5j30-fjk3-480fj39kl409",
 });
