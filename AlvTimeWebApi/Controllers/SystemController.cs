911d194ad0068ed99391a85fa29cc016e5b86661
Martin Skjønhaug <martin@alv.no>
2020-08-21 13:58:28 +0200

    Added system controller

---
+++ AlvTimeWebApi/Controllers/SystemController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AlvTimeWebApi.Controllers
+{
+    [ApiController]
+    public class SystemController : ControllerBase
+    {
+        [HttpGet("api/ping")]
+        public ActionResult Ping()
+        {
+            return Ok(DateTime.Now);
+        }
+
+        [HttpGet("api/throw")]
+        public ActionResult Throw()
+        {
+            throw new Exception("An error occured. This is for test purpose only");
+        }
+    }
+}
