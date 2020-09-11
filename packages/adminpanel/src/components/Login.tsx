83211cf6635b9089f5bbaaf4b81bc3213fc4e555
Truls Aagaard <truls@aagaard.io>
2020-09-11 20:34:25 +0200

    Update to latest msal-browser and rewrite flow

--- packages/adminpanel/src/components/Login.tsx
+++ packages/adminpanel/src/components/Login.tsx
@@ -1,40 +1,69 @@
-import { Account } from "@azure/msal-common";
-import React, { useState } from "react";
-import { login, logout } from "./../services/azureAd";
+import { AccountInfo } from "@azure/msal-common";
+import React, { useState, useEffect } from "react";
+import { getAllAccounts, login, logout } from "./../services/azureAd";
 
 const Json = ({ data }: any) => <pre>{JSON.stringify(data, null, 4)}</pre>;
 
 function Login() {
-  const [account, setAccount] = useState({} as Account);
+  const [accounts, setAccounts] = useState<AccountInfo[]>([]);
+  const [account, setAccount] = useState<AccountInfo>();
   const [error, setError] = useState("");
 
-  const onSignInClick = async () => {
+  const signIn = async () => {
+    const accounts = getAllAccounts();
+    if (accounts.length) {
+      setAccounts(accounts);
+      return;
+    }
+
     const loginResponse = await login().catch((error) => {
       setError(error.message);
     });
-    if (loginResponse && loginResponse.account) {
-      setAccount(loginResponse.account);
+
+    if (loginResponse) {
+      setAccounts(getAllAccounts());
     }
   };
 
-  return (
-    <div>
+  useEffect(() => {
+    signIn();
+  }, []);
+
+  if (!accounts.length)
+    return (
       <section>
         <h1>Login MVP</h1>
-        {!account.name ? (
-          <button onClick={onSignInClick}>Sign In</button>
+        {!accounts.length ? (
+          <button onClick={signIn}>Sign In</button>
         ) : (
           <button onClick={logout}>Sign Out</button>
         )}
         {error && <p className="error">Error: {error}</p>}
       </section>
+    );
+
+  if (!account)
+    return (
       <section>
-        {account.name && (
-          <div>
-            <h2>Session Account Data</h2>
-            <Json data={account} />
-          </div>
-        )}
+        <h1>Select Account</h1>
+        {accounts.map((account) => {
+          return (
+            <button key={account.username} onClick={() => setAccount(account)}>
+              {account.username}
+            </button>
+          );
+        })}
+      </section>
+    );
+
+  return (
+    <div>
+      <section>
+        <button onClick={logout}>Sign Out</button>
+        <div>
+          <h2>Session Account Data</h2>
+          <Json data={accounts} />
+        </div>
       </section>
     </div>
   );
