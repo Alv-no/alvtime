1fe532aad6e06e96f64d83378997302f4b519340
Truls Aagaard <truls@aagaard.io>
2020-09-24 21:41:34 +0200

    Remove MenuAppBar and Router

--- packages/adminpanel/src/App.tsx
+++ packages/adminpanel/src/App.tsx
@@ -1,21 +1,15 @@
 import { ThemeProvider } from "@material-ui/core";
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-// Views
-import MenuAppBar from "./components/MenuAppBar";
-import Tables from "./components/Tables";
 import Authentication from "./components/Login";
+import Tables from "./components/Tables";
 import theme from "./theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <Authentication>
-          <MenuAppBar />
-          <Tables />
-        </Authentication>
-      </Router>
+      <Authentication>
+        <Tables />
+      </Authentication>
     </ThemeProvider>
   );
 }
