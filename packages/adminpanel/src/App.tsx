f6001d4cd995a235f22c7521b43d0ba0d63e40de
Truls Aagaard <truls@aagaard.io>
2020-09-26 10:14:11 +0200

    Rename Login to AzureAdAuthenticator

--- packages/adminpanel/src/App.tsx
+++ packages/adminpanel/src/App.tsx
@@ -1,15 +1,15 @@
 import { ThemeProvider } from "@material-ui/core";
 import React from "react";
-import Authentication from "./components/Login";
+import AzureAdAuthenticator from "./components/AzureAdAuthenticator";
 import Tables from "./components/Tables";
 import theme from "./theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Authentication>
+      <AzureAdAuthenticator>
         <Tables />
-      </Authentication>
+      </AzureAdAuthenticator>
     </ThemeProvider>
   );
 }
