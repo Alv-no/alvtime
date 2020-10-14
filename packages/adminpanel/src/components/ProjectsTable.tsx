71d07ed456820323fa6c09938558d2a1ad6566d8
Truls Aagaard <truls@aagaard.io>
2020-10-14 20:30:49 +0200

    Add dropdown to ProjectsTable

--- packages/adminpanel/src/components/ProjectsTable.tsx
+++ packages/adminpanel/src/components/ProjectsTable.tsx
@@ -5,22 +5,36 @@ import { AlvtimeContext } from "../App";
 import { norsk } from "./norsk";
 import tableIcons from "./tableIcons";
 import { globalTableOptions, setCache } from "./Tables";
+import { TextField } from "@material-ui/core";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 export default function ProjectsTable() {
   const { alvtimeFetcher } = useContext(AlvtimeContext);
+  const { data: customers, error: customersLoadError } = useSWR(
+    "/api/admin/Customers"
+  );
   const columns: Column<object>[] = [
     { title: "Navn", field: "name", editable: "always", type: "string" },
     {
-      title: "Kunde Id",
-      field: "customer.id",
-      editable: "always",
-      type: "numeric",
-    },
-    {
-      title: "Kundenavn",
+      title: "Kunde",
       field: "customer.name",
-      editable: "never",
+      editable: "onAdd",
       type: "string",
+      editComponent: (props: any) => (
+        <Autocomplete
+          options={customers}
+          getOptionLabel={(option: { name: string }) => option.name}
+          onChange={(
+            _event: any,
+            newValue: { name: string; id: number } | null
+          ) => {
+            props.onChange(newValue ? newValue.id : 0);
+          }}
+          renderInput={(params: any) => {
+            return <TextField {...params} />;
+          }}
+        />
+      ),
     },
   ];
 
@@ -32,7 +46,13 @@ export default function ProjectsTable() {
     setCache(path, [...data, newData]);
     const addedData = await alvtimeFetcher(path, {
       method: "post",
-      body: [newData],
+      body: [
+        {
+          id: newData.id,
+          name: newData.name,
+          customer: newData.customer.name, // The Id is set in the name field in the Autocomplete
+        },
+      ],
     });
     setCache(path, [...addedData, ...data]);
   };
@@ -44,7 +64,9 @@ export default function ProjectsTable() {
     setCache(path, [...dataUpdate]);
     const updatedData = await alvtimeFetcher(path, {
       method: "put",
-      body: [newData],
+      body: [
+        { id: newData.id, name: newData.name, customer: newData.customer.id },
+      ],
     });
     dataUpdate[index] = updatedData[0];
     setCache(path, [...dataUpdate]);
