50d31e53bb15cfc57bc7154d63fb0decf9a33974
Pål Bøckmann <plbockmann@gmail.com>
2021-02-07 15:37:32 +0100

    Added endpoint for red days

---
+++ packages/api/AlvTimeWebApi/Controllers/HolidayController.cs
@@ -0,0 +1,28 @@
+﻿using AlvTime.Business;
+using AlvTimeWebApi.Controllers.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AlvTimeWebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class HolidayController : Controller
+    {
+        [HttpGet("Holidays")]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> FetchRedDays([FromQuery] int year)
+        {
+            var redDays = new RedDays(year);
+            var dates = new List<string>();
+
+            foreach (var date in redDays.Dates)
+            {
+                dates.Add(date.ToDateOnly());
+            }
+
+            return Ok(dates);
+        }
+    }
+}
