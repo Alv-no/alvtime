41b0eae521d20d1b1d89a270c21481cbe604da5d
Boye Svensson <boyevnsvensson@gmail.com>
2021-02-11 19:21:50 +0100

    Add hamburger menu component

---
+++ packages/frontend/src/components/Hamburger.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="{ mobile }" class="hamburger">
+    <YellowButton icon-id="menu" tooltip="Åpne meny" @click="toggleMenu" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import YellowButton from "./YellowButton.vue";
+export default Vue.extend({
+  components: {
+    YellowButton,
+  },
+
+  computed: {
+    mobile(): boolean {
+      // @ts-ignore
+      return this.$mq == "sm";
+    },
+  },
+
+  methods: {
+    toggleMenu() {
+      console.log("clickety-click!");
+      this.$store.commit("TOGGLE_DRAWER");
+    },
+  },
+});
+</script>
+
+<style scoped>
+.hamburger {
+  margin-right: 1rem;
+}
+
+.mobile {
+  margin-right: 0.5rem;
+}
+</style>
