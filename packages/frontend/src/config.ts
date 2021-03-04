8a364a74eddc93946fc5a7b604316cf5e7327635
Truls Aagaard <truls@aagaard.io>
2021-03-04 21:01:35 +0100

    Get config from file not env vars. This enables one docker image for all environments

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -1,23 +1,26 @@
-export default Object.freeze({
-  API_HOST: process.env.VUE_APP_API_HOST,
-  DATE_FORMAT: "YYYY-MM-DD",
-  HOURS_IN_WORKDAY: 7.5,
-  AUTHORITY: "https://login.microsoftonline.com/",
-  TENANT_ID: "76749190-4427-4b08-a3e4-161767dd1b73",
-  CLIENT_ID: process.env.VUE_APP_CLIENT_ID,
-  ACCESS_SCOPE: process.env.VUE_APP_ACCESS_SCOPE,
-  SLACK_TEAM_ID: "TJ70QLJRL",
-  SLACK_CHANNEL_ID: "CNEHG527J",
-  TEST_ACCESS_TOKEN: "5801gj90-jf39-5j30-fjk3-480fj39kl409",
-  BASE_URL_ADMINPANEL: fetchBaseUrl(),
-});
-
-function fetchBaseUrl() {
-  if (process.env.NODE_ENV === "development") {
-    return "http://localhost:3000";
-  } else if (window.location.host.includes("dev")) {
-    return "https://alvtime-admin-react-pwa-dev-as.azurewebsites.net#";
-  } else {
-    return "https://alvtime-admin-react-pwa-as.azurewebsites.net";
-  }
+interface Config {
+  API_HOST: string;
+  DATE_FORMAT: string;
+  HOURS_IN_WORKDAY: number;
+  AUTHORITY: string;
+  TENANT_ID: string;
+  CLIENT_ID: string;
+  ACCESS_SCOPE: string;
+  SLACK_TEAM_ID: string;
+  SLACK_CHANNEL_ID: string;
+  BASE_URL_ADMINPANEL: string;
 }
+
+let config = ({} as unknown) as Config;
+
+var request = new XMLHttpRequest();
+request.open("GET", "./config.json", false);
+request.send(null);
+
+if (request.status === 200 && request.responseText.length) {
+  config = JSON.parse(request.responseText);
+} else {
+  throw "Unable to fetch config";
+}
+
+export default config;
