fb9869bac01e830ecbdaf147c4d7190443ddf7a5
Pål Bøckmann <plbockmann@gmail.com>
2021-03-11 09:51:09 +0100

    Endpoint for getting used vacation days

--- packages/api/AlvTimeWebApi/Controllers/HolidayController.cs
+++ packages/api/AlvTimeWebApi/Controllers/HolidayController.cs
@@ -1,8 +1,13 @@
 ﻿using AlvTime.Business;
+using AlvTime.Business.Options;
+using AlvTime.Business.TimeEntries;
 using AlvTimeWebApi.Controllers.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AlvTimeWebApi.Controllers
 {
@@ -10,6 +15,18 @@ namespace AlvTimeWebApi.Controllers
     [ApiController]
     public class HolidayController : Controller
     {
+        private readonly IOptionsMonitor<TimeEntryOptions> _timeEntryOptions;
+        private RetrieveUsers _userRetriever;
+        private readonly ITimeEntryStorage _timeEntryStorage;
+
+        public HolidayController(RetrieveUsers userRetriever, IOptionsMonitor<TimeEntryOptions> timeEntryOptions, ITimeEntryStorage timeEntryStorage)
+        {
+            _userRetriever = userRetriever;
+            _timeEntryOptions = timeEntryOptions;
+            _timeEntryStorage = timeEntryStorage;
+
+        }
+
         [HttpGet("Holidays")]
         [Authorize]
         public ActionResult<IEnumerable<string>> FetchRedDays([FromQuery] int year)
@@ -23,6 +40,30 @@ namespace AlvTimeWebApi.Controllers
             }
 
             return Ok(dates);
+        }
+
+        [HttpGet("user/UsedVacationDays")]
+        [Authorize(Policy = "AllowPersonalAccessToken")]
+        public ActionResult<IEnumerable<TimeEntriesResponseDto>> FetchUsedVacationDays([FromQuery] int year)
+        {
+            var user = _userRetriever.RetrieveUser();
+
+            return Ok(_timeEntryStorage.GetTimeEntries(new TimeEntryQuerySearch
+            {
+                FromDateInclusive = new DateTime(year, 01, 01),
+                ToDateInclusive = new DateTime(year, 12, 31),
+                UserId = user.Id,
+                TaskId = _timeEntryOptions.CurrentValue.PaidHolidayTask
+            })
+            .Select(timeEntry => new
+            {
+                User = timeEntry.User,
+                UserEmail = timeEntry.UserEmail,
+                Id = timeEntry.Id,
+                Date = timeEntry.Date.ToDateOnly(),
+                Value = timeEntry.Value,
+                TaskId = timeEntry.TaskId
+            }));
         }
     }
 }
