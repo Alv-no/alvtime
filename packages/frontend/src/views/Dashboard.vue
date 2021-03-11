829126501b01f606126bf37bd19f65dcf3fcfebe
Martin Skjønhaug <martin.skjoenhaug@gmail.com>
2021-03-11 20:26:33 +0100

    Started working on bindestrekbrettet

---
+++ packages/frontend/src/views/Dashboard.vue
@@ -0,0 +1,35 @@
+<template>
+  <CenterColumnWrapper>
+    <h1>Halla, dashboard her</h1>
+    <div class="chart-container">
+      <Chart class="chart"></Chart>
+      <Chart class="chart"></Chart>
+      <Chart class="chart"></Chart>
+    </div>
+  </CenterColumnWrapper>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Chart from "@/components/CommitChart.vue";
+import CenterColumnWrapper from "@/components/CenterColumnWrapper.vue";
+
+export default Vue.extend({
+  name: "Home",
+  components: {
+    Chart,
+    CenterColumnWrapper,
+  },
+});
+</script>
+
+<style scoped>
+.chart-container {
+  display: flex;
+}
+
+.chart {
+  width: 30%;
+  margin: 0 10px;
+}
+</style>
