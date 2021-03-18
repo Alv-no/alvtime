cc72b9dd5dd4fa54d193a3645ffa6db6ed7367dc
Pål Bøckmann <plbockmann@gmail.com>
2021-03-18 18:47:01 +0100

    Chart with real data

--- packages/frontend/src/views/Dashboard.vue
+++ packages/frontend/src/views/Dashboard.vue
@@ -2,7 +2,7 @@
   <CenterColumnWrapper>
     <h1>Halla, dashboard her</h1>
     <div class="chart-container">
-      <Chart class="chart"></Chart>
+      <Chart class="chart" :data="monthSums"></Chart>
       <Chart class="chart"></Chart>
       <Chart class="chart"></Chart>
     </div>
@@ -13,12 +13,35 @@
 import Vue from "vue";
 import Chart from "@/components/CommitChart.vue";
 import CenterColumnWrapper from "@/components/CenterColumnWrapper.vue";
+import moment from "moment";
 
 export default Vue.extend({
   name: "Home",
   components: {
     Chart,
     CenterColumnWrapper,
+  },
+  beforeCreate() {
+    this.$store.dispatch("FETCH_WEEK_ENTRIES");
+  },
+  computed: {
+    monthSums() {
+      const timeEntries = this.$store.state.timeEntries;
+      if (!timeEntries) return;
+
+      const monthSums: number[] = Array(12).fill(0);
+      const thisYear = moment().year();
+
+      for (const timeEntry of timeEntries) {
+        const timeEntryYear = moment(timeEntry.date).year();
+        const month = moment(timeEntry.date).month();
+        const value: number = timeEntry.value;
+        if (timeEntryYear === thisYear) {
+          monthSums[month] += Number(value);
+        }
+      }
+      return monthSums;
+    },
   },
 });
 </script>
