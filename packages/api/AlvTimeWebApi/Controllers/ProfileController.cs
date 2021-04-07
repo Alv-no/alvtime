4a9e05bca6cd65fe16a05ba999ad2661a35adf7d
Truls Aagaard <truls@aagaard.io>
2021-04-07 21:10:05 +0200

    Add UsersReport endpoint

--- packages/api/AlvTimeWebApi/Controllers/ProfileController.cs
+++ packages/api/AlvTimeWebApi/Controllers/ProfileController.cs
@@ -1,7 +1,11 @@
-﻿using AlvTime.Persistence.DataBaseModels;
+﻿using AlvTime.Business.Options;
+using AlvTime.Business.Users;
+using AlvTime.Persistence.DataBaseModels;
 using AlvTimeWebApi.Controllers.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
 
 namespace AlvTimeWebApi.Controllers
 {
@@ -10,10 +14,14 @@ namespace AlvTimeWebApi.Controllers
     public class ProfileController : Controller
     {
         private RetrieveUsers _userRetriever;
+        private readonly IUserStorage _userStorage;
+        private readonly int _reportUser;
 
-        public ProfileController(RetrieveUsers userRetriever)
+        public ProfileController(RetrieveUsers userRetriever, IUserStorage userStorage, IOptionsMonitor<TimeEntryOptions> timeEntryOptions)
         {
             _userRetriever = userRetriever;
+            _userStorage = userStorage;
+            _reportUser = timeEntryOptions.CurrentValue.ReportUser;
         }
 
         [HttpGet("Profile")]
@@ -21,6 +29,21 @@ namespace AlvTimeWebApi.Controllers
         public ActionResult<User> GetUserProfile()
         {
             return Ok(_userRetriever.RetrieveUser());
+        }
+
+        [HttpGet("UsersReport")]
+        [Authorize(Policy = "AllowPersonalAccessToken")]
+        public ActionResult<IEnumerable<UserResponseDto>> FetchUsersReport()
+        {
+          var user = _userRetriever.RetrieveUser();
+
+          if (user.Id == _reportUser)
+          {
+            var users = _userStorage.GetUser(new UserQuerySearch());
+            return Ok(users);
+          }
+
+          return Unauthorized();
         }
     }
 }
