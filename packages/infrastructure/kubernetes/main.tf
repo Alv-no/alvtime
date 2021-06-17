e681d916d3f13ed3da53f55a557a7edb7558a2c0
Truls Aagaard <truls@aagaard.io>
2021-06-17 13:44:26 +0200

    Update kubernetes version to 1.20.7

--- packages/infrastructure/kubernetes/main.tf
+++ packages/infrastructure/kubernetes/main.tf
@@ -17,14 +17,14 @@ resource "azurerm_kubernetes_cluster" "this" {
   resource_group_name = var.resource_group_name
   dns_prefix          = "${var.name}-k8s"
   sku_tier            = "Free"
-  kubernetes_version  = "1.19.11"
+  kubernetes_version  = "1.20.7"
 
   default_node_pool {
     name                 = "default"
     node_count           = 2
     vm_size              = "Standard_D2_v2"
     os_disk_size_gb      = 30
-    orchestrator_version = "1.19.11"
+    orchestrator_version = "1.20.7"
   }
 
   service_principal {
