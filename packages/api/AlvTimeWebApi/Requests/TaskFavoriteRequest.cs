3d0aad244f30d4a5f23d1960da1a76395b8b9c7c
Pål <plbockmann@gmail.com>
2022-01-20 16:12:30 +0100

    Fix rebase conflict

---
+++ packages/api/AlvTimeWebApi/Requests/TaskFavoriteRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace AlvTimeWebApi.Requests
+{
+    public class TaskFavoriteRequest
+    {
+        public int Id { get; set; }
+        public bool Favorite { get; set; }
+        public bool? Locked { get; set; }
+        public string Name { get; set; }
+        public decimal? CompensationRate { get; set; }
+    }
+}
