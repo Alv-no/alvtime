6fd8438c3dcf12ebd495ec1fc9a0201c2003e893
Truls Aagaard <truls@aagaard.io>
2022-01-22 22:06:17 +0100

    Fortell de som var med at læring har blitt registrert

--- packages/slack-app/src/routes/learning-collector/blocks.ts
+++ packages/slack-app/src/routes/learning-collector/blocks.ts
@@ -1,7 +1,11 @@
-export function plainText(text: string) {
+export function plainText(text: string): {
+  type: "plain_text";
+  text: string;
+  emoji: boolean;
+} {
   return { type: "plain_text", text, emoji: true };
 }
 
-export function markdown(text: string) {
+export function markdown(text: string): { type: "mrkdwn"; text: string } {
   return { type: "mrkdwn", text };
 }
