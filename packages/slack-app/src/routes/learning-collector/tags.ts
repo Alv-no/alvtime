deee4e078e66d298c49500ac78aca59605f6775c
Truls Aagaard <truls@aagaard.io>
2022-01-23 00:39:30 +0100

    Update tags on start

--- packages/slack-app/src/routes/learning-collector/tags.ts
+++ packages/slack-app/src/routes/learning-collector/tags.ts
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 import { logger } from "../../createLogger";
 import tagsDB, { Tag } from "../../models/tags";
+import mockTags from "./cvpartnerTags";
 
 async function fetchBatchFromCVPartner(offset: number): Promise<Tag[]> {
   const token = process.env.CVPARTNER_API_TOKEN;
@@ -15,20 +16,27 @@ async function fetchBatchFromCVPartner(offset: number): Promise<Tag[]> {
 
 export async function updateFromCVPartner() {
   logger.info("Starting update of tags form CVPartner");
-  const savedTags = await tagsDB.getAll();
   let offset = 0;
   let countFetchedLast = 100;
   do {
     const cvPartnerTags = await fetchBatchFromCVPartner(offset);
+    const newTags: Tag[] = [];
     for (const tag of cvPartnerTags) {
-      const exists = savedTags.some((savedTag) => savedTag.term === tag.term);
-      if (!exists) {
-        const savedTag = await tagsDB.save(tag);
-        logger.info(`Saved tag: ${savedTag.term}`);
-      }
+      const exists = await tagsDB.exists(tag.term);
+      if (!exists) newTags.push(tag);
     }
+    const innsertedTags = await tagsDB.batchInsert(newTags);
+    logger.info(`Saved mock ${innsertedTags.length} tags to DB`);
     countFetchedLast = cvPartnerTags.length;
     offset = offset + 100;
   } while (countFetchedLast === 100);
   logger.info("Finished updating tags from CVPartner");
+}
+
+export async function updateFromMockTags() {
+  const tagsInDB = await tagsDB.count();
+  logger.info(`Number of tags in DB is ${tagsInDB}`);
+  if (tagsInDB) return;
+  const innsertedTags = await tagsDB.batchInsert(mockTags);
+  logger.info(`Saved mock ${innsertedTags.length} tags to DB`);
 }
