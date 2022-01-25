dd90bb483e033c0071bc5de0fd644e37cd85a647
Boye Svensson <boyevnsvensson@gmail.com>
2022-01-25 20:39:20 +0100

    Create file for all reply texts

---
+++ packages/slack-app/src/routes/learning-collector/responses.ts
@@ -0,0 +1,24 @@
+import { getRandomNumber } from "./reactions";
+
+export const getResponseMessage = (messageType: string) => {
+    switch(messageType) {
+        case 'whatUserIsLearningQuestion': {
+            return whatUserIsLearningQuestion[getRandomNumber(whatUserIsLearningQuestion.length)];
+        }
+        case 'learnMoreText': {
+            return learnMoreText[getRandomNumber(learnMoreText.length)];
+        }
+    }
+};
+
+const whatUserIsLearningQuestion: Array<string> = [
+    "Trykk på knappen og fortell meg hva du lærer deg for tiden :wave:",
+    "Har du lest, sett eller snakket om noe fett i det siste? La folket få vite det! :bulb:",
+    "Vår kunnskapsbrønn trenger påfyll og lengter etter din hjernebarks oppdaterte signaler! :brain:"
+]; 
+
+const learnMoreText: Array<string> = [
+    "Sugen på en nærmere titt? Sjekk linken!",
+    "Nysgjerrig? Finn ut mer her!",
+    "Vi vet du ble inspirert. Sjekk det ut nærmere her!"
+];
