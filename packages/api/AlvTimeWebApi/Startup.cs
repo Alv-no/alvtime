8d583f6a3c6fa92374cf4e59c2442728224bb822
Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>
2022-04-20 09:18:05 +0200

    Extract Swagger configuration to its own file

--- packages/api/AlvTimeWebApi/Startup.cs
+++ packages/api/AlvTimeWebApi/Startup.cs
@@ -43,75 +43,7 @@ namespace AlvTimeWebApi
             services.AddAlvtimeAuthentication(Configuration);
             services.Configure<TimeEntryOptions>(Configuration.GetSection("TimeEntryOptions"));
             services.AddAlvtimeAuthorization();
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Alvtime API", Version = "v1"});
-
-                // AAD SSO
-                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-                {
-                    Description = "Authenticate using Azure AD with your account.",
-                    Name = "Azure AD",
-                    Type = SecuritySchemeType.OAuth2,
-                    Flows = new OpenApiOAuthFlows
-                    {
-                        AuthorizationCode = new OpenApiOAuthFlow
-                        {
-                            AuthorizationUrl =
-                                new Uri(
-                                    $"https://login.microsoftonline.com/{Configuration["AzureAd:TenantId"]}/oauth2/v2.0/authorize"),
-                            TokenUrl = new Uri(
-                                $"https://login.microsoftonline.com/{Configuration["AzureAd:TenantId"]}/oauth2/v2.0/token"),
-                            Scopes = new Dictionary<string, string>
-                            {
-                                {Configuration["AzureAd:Domain"], "Access the API"}
-                            }
-                        }
-                    }
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference {Type = ReferenceType.SecurityScheme, Id = "oauth2"}
-                        },
-                        new[] {Configuration["AzureAd:Domain"]}
-                    }
-                });
-
-                // Bearer token (for use with debugging and Ahre-Ketil Lillehagen)
-                c.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
-                {
-                    Name = "Bearer token",
-                    Description = "Authenticate using a raw bearer token. This is helpful when you want to use Ahre-Ketil for testing. You can paste his PAT here.",
-                    Scheme = "Bearer",
-                    BearerFormat = "Raw JWT or PAT",
-                    Type = SecuritySchemeType.Http,
-                    Flows = new OpenApiOAuthFlows
-                    {
-                         ClientCredentials = new OpenApiOAuthFlow
-                         {
-                             Scopes = new Dictionary<string, string>
-                             {
-                                 {Configuration["AzureAd:Domain"], "Access the API"}
-                             }
-                         }
-                    },
-                    In = ParameterLocation.Header,
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference {Type = ReferenceType.SecurityScheme, Id = "bearer"}
-                        },
-                        new[] {Configuration["AzureAd:Domain"]}
-                    }
-                });
-            });
+            services.AddSwaggerExtensions(Configuration);
             services.AddRazorPages();
             services.AddAlvtimeCorsPolicys(Configuration);
         }
