49ba8173b7008a4d0c69fe112b435b940eea777c
Fredrik Jenssen <fredrik.jenssen@alv.no>
2023-08-07 14:22:01 +0200

    626: added percentage symbol to y axis labels

--- packages/frontend/src/components/InvoiceChart.vue
+++ packages/frontend/src/components/InvoiceChart.vue
@@ -78,8 +78,12 @@ export default Vue.extend({
         scales: {
           y: {
             stacked: true,
+            ticks: {
+                callback: (value: string|number, _index: number, _ticks: unknown[]) => {
+                    return `${value} %`;
+                }
+            },
           },
-
           x: {
             stacked: true,
           },
