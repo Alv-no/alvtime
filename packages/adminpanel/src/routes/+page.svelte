a071262c6d6220c4eb9753ce032dcd430aea9d4f
Øyvind Schjerven <oyvind.jalland@hotmail.no>
2023-08-16 10:59:35 +0200

    added basic routing

--- packages/adminpanel/src/routes/+page.svelte
+++ packages/adminpanel/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button>
+    <a href="/customers">Kunder</a>
+</button>
+
+<button>
+    <a href="/employees">Ansatte</a>
+</button>
---
+++ packages/adminpanel/src/routes/customers/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Kunder</h1>
+
+<button>
+    <a href="/customers/statistics">{stat}</a>
+</button>
+
+<script lang="ts">
+    const stat = "Statistikk er kult"
+</script>
+
