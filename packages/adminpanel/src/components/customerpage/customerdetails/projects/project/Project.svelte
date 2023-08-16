f85fc4e83002cf7d8141648ee8313f1bd289dfd4
StormFlaate <fflaate@hotmail.no>
2023-08-16 13:52:51 +0200

    Wip - Creates mock version of forslag 1.2 - refactor for improved component structure - Made lineshifts, etc, consistent - Oppdaterte komponentstrukturen etter nyeste design

---
+++ packages/adminpanel/src/components/customerpage/customerdetails/projects/project/Project.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type {TProject} from "../../../../../lib/types.ts"
+    export let project : TProject
+    export let selectProject : Function
+</script>
+
+
+<li class="project-item">
+    <button on:click={() => selectProject(project.id)} class="button">
+        {project.name}
+    </button>
+</li>
---
+++ packages/adminpanel/src/components/customerpage/customerlist/CustomerList.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Customers from "./customers/Customers.svelte";
+    import SearchBar from "./searchbar/SearchBar.svelte";
+    import type { TCustomer } from "$lib/types";
+
+    export let customers : TCustomer[]
+    export let selectCustomer : Function
+
+
+</script>
+<div class="customerlist">
+    <SearchBar />
+    <Customers {customers} {selectCustomer} />
+</div>
+
+<style>
+    .customerlist {
+        width: 16.67%; /* 2 out of 12 columns */
+        background-color: #f0f0f0;
+        padding: 10px;
+        box-sizing: border-box;
+        border-right: 1px solid #ccc;
+        height: 100vh; /* Full viewport height */
+    }
+</style>
