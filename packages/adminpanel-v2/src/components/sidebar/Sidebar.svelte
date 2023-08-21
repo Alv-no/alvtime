a899b0bc3df480e1a42b6f2fec6bb8ef44225ce1
Kristian Nilsen <kristian@alv.no>
2023-08-21 16:23:52 +0200

    Added sidebar menu

---
+++ packages/adminpanel-v2/src/components/sidebar/Sidebar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import 'iconify-icon'
+
+    export let page_name : string 
+    export let page_route : string
+    export let icon : string
+</script>
+
+<a href={page_route} class="Sidebar-entry no-underline hover:underline">
+    <iconify-icon icon={icon} class="text-alv-dark-blue" width="30px"></iconify-icon>
+    <div class="text-alv-dark-blue">
+        {page_name}
+    </div>
+    
+</a>
+
+<style>
+    .Sidebar-entry {
+        display: flex;
+        vertical-align: center;
+    }
+</style>
