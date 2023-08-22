70d6bcaf50d800ed9e5f54ea3beb4484e93be9af
StormFlaate <fflaate@hotmail.no>
2023-08-22 12:20:09 +0200

    Refactors component structure and adds searchbar for a refactored customer list
    - Refactors the customer header list to the correct place in accordance with figma design
    - Refactors component structure by removing unecessary SearchBar component
    - Creates search input field for customer list
    - Refactors visually the design of the customer list
    
    Creates searchbar to filter Customers and updates the visual representation of the customer list

--- packages/adminpanel-v2/src/components/customerpage/customerdetails/headers/ListHeader.svelte
+++ packages/adminpanel-v2/src/components/customerpage/customerdetails/headers/ListHeader.svelte
@@ -1,27 +1,18 @@
 <script lang="ts">
 	import ListHeaderText from "./ListHeaderText.svelte";
 
-
-
-    let customerHeaderTexts: string[] = ["Kunde"]
     let projectHeaderTexts: string[] = ["Prosjekt"]
     let activityHeaderTexts: string[] = ["Aktivitet", "Pris", "Overtidsfaktor"]
 </script>
 
 <div class="grid-container grid grid-cols-12 items-center bg-slate-50">
-    
-    <!-- Customer header list-->
-    <div class="col-span-2 flex justify-between border-r-2 border-b-2 border-slate-300">
-        <ListHeaderText headerTexts={customerHeaderTexts} />
-    </div>
-
     <!-- Project header list-->
-    <div class="col-span-3 flex justify-between border-r-2 border-b-2 border-slate-300">
+    <div class="col-span-2 flex justify-between border-r border-b border-slate-300">
         <ListHeaderText headerTexts={projectHeaderTexts} />
     </div>
 
     <!-- Activity header list-->
-    <div class="col-span-7 flex justify-between border-r-2 border-b-2 border-slate-300">
+    <div class="col-span-10 flex justify-between border-r border-b border-slate-300">
         <ListHeaderText headerTexts={activityHeaderTexts} />
     </div>
     
