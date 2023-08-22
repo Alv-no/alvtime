5772d4910d7c22e80691699eaa6036fdcc7c1a44
StormFlaate <fflaate@hotmail.no>
2023-08-22 15:04:44 +0200

    creates project list component
    - includes marking the chosen project
    - includes visually updating the project list

--- packages/adminpanel-v2/src/components/customerpage/customerlist/customers/customer/Customer.svelte
+++ packages/adminpanel-v2/src/components/customerpage/customerlist/customers/customer/Customer.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
     import type {TCustomer} from "../../../../../lib/types.ts"
+	import { customers } from "../../../../../stores/CustomerStore.ts";
     export let customer : TCustomer
     export let selectCustomer : Function
 </script>
 
 
-<button on:click={() => selectCustomer(customer.id)} class="border-r border-b border-gray-300 bg-white w-full flex">
-    <div class="p-4">
-        <span class="text-sm  text-gray-800">{customer.name}</span>
-    </div>
-</button>
+{#if customer.id == customers.getActiveCustomer()?.id}
+    <button 
+        on:click={() => selectCustomer(customer.id)} 
+        class="border-2 border-blue-500 bg-sky-50 w-full flex"
+    >
+        <div class="p-4">
+            <span class="text-sm  text-gray-800">{customer.name}</span>
+        </div>
+    </button>
+{:else}
+    <button on:click={() => selectCustomer(customer.id)} class="border-r border-b border-gray-300 bg-white w-full flex">
+        <div class="p-4">
+            <span class="text-sm  text-gray-800">{customer.name}</span>
+        </div>
+    </button>
+{/if}
