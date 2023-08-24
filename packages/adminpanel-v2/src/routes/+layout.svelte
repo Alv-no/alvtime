5d0c294ea69e73b0df95ca0edb34a0b10b2c20c0
Johan August Østbye <johan@ostbye.dev>
2023-08-24 16:40:49 +0200

    Finished styling the sidebar

--- packages/adminpanel-v2/src/routes/+layout.svelte
+++ packages/adminpanel-v2/src/routes/+layout.svelte
@@ -1,39 +1,10 @@
 <script>
 	import '../app.css';
-    import 'iconify-icon'
-    import SidebarEntry from "../components/sidebar/Sidebar.svelte"
-
-    let hover = false
+	import 'iconify-icon';
+	import Sidebar from '../components/sidebar/Sidebar.svelte';
 </script>
 
-<div class="wrapper">
-    <nav on:mouseenter={() => {
-        hover = true;
-    }}
-    on:mouseleave={() => {
-        hover = false;
-    }}>
-        <div class="container">
-            {#if hover}
-                <SidebarEntry page_name={"Home"} page_route={"/"} icon={"majesticons:home"}/>
-                <SidebarEntry page_name={"Kunder"} page_route={"/customers"} icon={"ic:round-corporate-fare"}/>
-                <SidebarEntry page_name={"Ansatte"} page_route={"/employees"} icon={"fluent:person-20-filled"}/>
-            {:else}
-                <SidebarEntry page_name={""} page_route={"/"} icon={"ph:plus-fill"}/>
-            {/if}
-            </div>
-    </nav>
-
-    <slot />
+<div class="flex h-full">
+	<Sidebar />
+	<slot />
 </div>
-
-<style>
-    .container {
-        display: flex;
-        flex-direction: column;
-    }
-
-    .wrapper {
-        display: flex;
-    }
-</style>
