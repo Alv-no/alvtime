1bbb516d18aa14c8b999eaacc6df864dc82d9f6c
StormFlaate <fflaate@hotmail.no>
2023-08-25 12:47:27 +0200

    Added functionality for adding new tasks
    - Create new task
    - Create new hour rate for the task
    - Create new compensation rate for the task
    - Add new objects to the store
    - Create new hour rate and set end date for old rate when updating
    - Create new compensation rate and set end date for old rate when updating
    - Minor styling fix for compensation rate input field

--- packages/adminpanel-v2/src/components/customerpage/customerdetails/tasks/task/Task.svelte
+++ packages/adminpanel-v2/src/components/customerpage/customerdetails/tasks/task/Task.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import type { TTask } from '../../../../../lib/types.ts';
-	import { customers } from '../../../../../stores/CustomerStore.ts';
-	import EditButton from '../../../../generic/buttons/EditButton.svelte';
-	export let task: TTask;
-	let isEditingTask: boolean = false;
+    import type { TTask } from "../../../../../lib/types.ts"
+    import { customers } from "../../../../../stores/CustomerStore.ts";
+	import EditButton from "../../../../generic/buttons/EditButton.svelte";
+    export let task: TTask;
+    let isEditingTask: boolean = false;
 
-	$: inactive = task.endDate ? true : false;
-	$: hourRate = $customers.hourRate.find((h) => h.taskId == task.id)!;
-	$: compensationRate = $customers.compensationRate.find((c) => c.taskId == task.id)!;
+    $: inactive = task.endDate ? true : false
+    let hourRate = $customers.hourRate.find(h => h.taskId == task.id && !h.endDate)!.rate
+    let compensationRate = $customers.compensationRate.find(c => c.taskId == task.id && !c.endDate)!.value
 
 	$: taskStyling = inactive
 		? 'border-r border-b border-gray-300 bg-gray-100 w-full flex justify-center p-4 grid grid-cols-12 gap-4'
@@ -17,42 +17,49 @@
 		? 'w-full col-span-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500 disabled:bg-transparent'
 		: 'w-full col-span-3 bg-transparent rounded border border-transparent';
 
-	$: hourRateStyling = isEditingTask
-		? 'col-span-2 bg-transparent rounded border border-gray-300 focus:outline-none focus:border-blue-500 disabled:bg-transparent'
-		: 'col-span-2 bg-transparent rounded border border-transparent';
+    $: hourRateStyling = isEditingTask ? "col-span-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500 disabled:bg-transparent" 
+        : "col-span-2 bg-transparent rounded border border-transparent"
 
-	$: compensationRateStyling = isEditingTask
-		? 'col-span-2 bg-transparent rounded border border-gray-300 focus:outline-none focus:border-blue-500 disabled:bg-transparent'
-		: 'col-span-2 bg-transparent rounded border border-transparent';
+    $: compensationRateStyling = isEditingTask ? "col-span-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500 disabled:bg-transparent" 
+        : "col-span-2 bg-transparent rounded border border-transparent"
 
-	$: editButtonStyling = isEditingTask
-		? 'rounded border-2 justify-center align-center border-blue-500'
-		: 'rounded border-2 justify-center align-center border-transparent';
-
-	let editTask = () => {
-		isEditingTask = !isEditingTask;
-		if (isEditingTask) {
-			customers.setTask(task.id);
-			customers.updateTask(task, hourRate, compensationRate);
-		}
-	};
+    let editTask = () => {
+        if (isEditingTask) {
+            if (hourRate && compensationRate) {
+                customers.setTask(task.id);
+                customers.updateTask(task, hourRate!, compensationRate!);
+                isEditingTask = !isEditingTask
+            }
+            else {
+                throw new Error("Not in valid state of program, task must have valid hour rate and compensation rate");
+            }
+        }
+        else {
+            isEditingTask = !isEditingTask;
+        }
+    }
+    
 </script>
 
-<!-- LEGGE INN NY AKTIVTET!!! -->
-<!-- Currently editing -->
 
-<button class={taskStyling}>
-	<input type="text" disabled={!isEditingTask} class={nameStyling} bind:value={task.name} />
-	<input type="text" class={hourRateStyling} bind:value={hourRate.rate} disabled={!isEditingTask} />
-
-	<span class="col-span-2" />
-	<input
-		type="text"
-		class={compensationRateStyling}
-		bind:value={compensationRate.value}
-		disabled={!isEditingTask}
-	/>
-	<span class="col-span-2" />
-
+<button 
+    class={taskStyling}>
+    <input type="text" disabled={!isEditingTask} class={nameStyling} bind:value={task.name} />
+    <input
+        type="number"
+        class={hourRateStyling}
+        bind:value={hourRate}
+        disabled={!isEditingTask}
+    />
+    
+    
+    <span class="col-span-2"></span>
+    <input
+        type="number"
+        class={compensationRateStyling}
+        bind:value={compensationRate}
+        disabled={!isEditingTask} 
+    />
+    <span class="col-span-2"></span>
 	<EditButton updateFunction={editTask} isDisabled={false} />
 </button>
