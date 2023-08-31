ef82481732d203628acc549bc695d561f76a8900
Pål Bøckmann <pal@alv.no>
2023-08-31 19:34:01 +0200

    Added support for EF migrations

--- packages/api/AlvTimeWebApi/Program.cs
+++ packages/api/AlvTimeWebApi/Program.cs
@@ -1,12 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using AlvTime.MigrationClient;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace AlvTimeWebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private readonly IConfiguration _configuration;
+
+        public Program(IConfiguration configuration)
         {
+            _configuration = configuration;
+        }
+        
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var environment = builder.Environment;
+            var connectionString = builder.Configuration.GetConnectionString("AlvTime_db");
+            if (connectionString != null) await MigrationClient.RunMigrations(connectionString, shouldSeed: environment.IsDevelopment());
             CreateHostBuilder(args).Build().Run();
         }
 
