951add124a9f70bf45c2e736cffd9d05521c2a4b
sofie <sofie@alv.no>
2023-10-30 10:48:20 +0100

    added controller for getting time entries for multiple people and tasks

---
+++ packages/api/AlvTime.Persistence/Repositories/MultipleTimeEntryQueryableExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using AlvTime.Business.TimeRegistration;
+using AlvTime.Persistence.DatabaseModels;
+
+namespace AlvTime.Persistence.Repositories
+{
+    public static class MultipleTimeEntryQueryableExtension
+    {
+        public static IQueryable<Hours> Filter(this IQueryable<Hours> query, MultipleTimeEntriesQuerySearch criterias)
+        {
+            if (criterias.EmployeeIds.Any())
+            {
+                query = query.Where(hour => criterias.EmployeeIds.Any(user => user == hour.UserNavigation.EmployeeId));
+            }
+            if (criterias.FromDateInclusive != null)
+            {
+                query = query.Where(hour => hour.Date.Date >= criterias.FromDateInclusive);
+            }
+            if (criterias.ToDateInclusive != null)
+            {
+                query = query.Where(hour => hour.Date.Date <= criterias.ToDateInclusive);
+            }
+            if (criterias.TaskIds.Any())
+            {
+                query = query.Where(hour => criterias.TaskIds.Any(task => task == hour.TaskId));
+            }
+
+            return query;
+        }
+    }
+}
---
+++ packages/api/AlvTimeWebApi/Controllers/Admin/TimeEntriesAdminController.cs
@@ -0,0 +1,55 @@
+﻿using AlvTime.Business.TimeRegistration;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using AlvTimeWebApi.Authentication;
+using System.Linq;
+using AlvTimeWebApi.Utils;
+
+namespace AlvTimeWebApi.Controllers.Admin
+{
+    [Route("api/admin")]
+    [ApiController]
+    [AuthorizeAdmin]
+    public class TimeEntriesAdminController : Controller
+    {
+        private readonly ITimeRegistrationStorage _storage;
+
+        public TimeEntriesAdminController(ITimeRegistrationStorage storage)
+        {
+            _storage = storage;
+        }
+
+        [HttpGet("TimeEntries")]
+        public async Task<ActionResult<IEnumerable<TimeEntryEmployeeResponseDto>>> GetTimeEntriesForEmployees([FromQuery] int[] employeeIds, [FromQuery] int[] taskIds, DateTime fromDate, DateTime toDate)
+        {
+            try
+            {
+                return Ok((await _storage.GetTimeEntriesForEmployees(new MultipleTimeEntriesQuerySearch
+                {
+                    EmployeeIds = employeeIds,
+                    FromDateInclusive = fromDate,
+                    ToDateInclusive = toDate,
+                    TaskIds = taskIds
+                }))
+                    .Select(timeEntry => new
+                    {
+                        User = timeEntry.User,
+                        EmployeeId = timeEntry.EmployeeId,
+                        Date = timeEntry.Date.ToDateOnly(),
+                        Value = timeEntry.Value,
+                        TaskId = timeEntry.TaskId,
+                        ProjectId = timeEntry.ProjectId,
+                    }));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new
+                {
+                    Message = e.ToString(),
+                });
+            }
+        }
+    }
+}
