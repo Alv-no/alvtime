0a8874ee560eaf0b8d3431649e69b12d9ad651c6
Pål Bøckmann <pal@alv.no>
2024-03-12 11:40:06 +0100

    Lagt til orgnr på customer

---
+++ packages/api/AlvTime.Persistence/Migrations/20240312103659_Customer_AddOrgNr.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlvTime.Persistence.Migrations
+{
+    public partial class Customer_AddOrgNr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrgNr",
+                table: "Customer",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrgNr",
+                table: "Customer");
+        }
+    }
+}
