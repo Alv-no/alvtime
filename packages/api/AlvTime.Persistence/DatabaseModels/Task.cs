ef781c98db8e18fc82a219f699f2c20062533f48
Pål Bøckmann <plbockmann@gmail.com>
2024-07-01 12:58:58 +0200

    Description på task kan være null

--- packages/api/AlvTime.Persistence/DatabaseModels/Task.cs
+++ packages/api/AlvTime.Persistence/DatabaseModels/Task.cs
@@ -15,7 +15,7 @@ namespace AlvTime.Persistence.DatabaseModels
 
         public int Id { get; set; }
         public string Name { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
         public int Project { get; set; }
         public bool Locked { get; set; }
         public bool Favorite { get; set; }
