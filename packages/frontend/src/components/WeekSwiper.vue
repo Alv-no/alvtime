ba6384c17d8821af3e65802e34428d93b7b471bd
Eirik Steira <eirik.steira@alv.no>
2024-09-05 12:47:36 +0200

    Adjust swiper container height to ensure comment box does not appear behind it

--- packages/frontend/src/components/WeekSwiper.vue
+++ packages/frontend/src/components/WeekSwiper.vue
@@ -75,13 +75,7 @@ export default Vue.extend({
 
 <style>
 .swiper-container {
-  overflow-y: visible !important; /* Ensure comment box is not hidden behind the swiper */
-  min-height: 50vh !important;
-}
-
-@media only screen and (max-width: 1000px) {
-  .center {
-    overflow: visible !important;
-  }
+  /* Ensure comment box is not hidden behind the swiper */
+  min-height: 85vh;
 }
 </style>
