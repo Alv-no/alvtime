818a6573a5d145f4b50918728847d0a0d0650b07
Eirik Steira <eirik.steira@alv.no>
2024-09-05 10:33:37 +0200

    Support touch events in click outside directive

--- packages/frontend/src/directives/click-outside.ts
+++ packages/frontend/src/directives/click-outside.ts
@@ -1,25 +1,29 @@
 import { DirectiveOptions } from "vue/types/options";
 
 interface ClickOutsideElement extends HTMLElement {
-  clickOutsideEvent?: (event: MouseEvent) => void;
+  clickOutsideEvent?: (event: MouseEvent | TouchEvent) => void;
 }
 
 const clickOutsideDirective: DirectiveOptions = {
   bind(el: ClickOutsideElement, binding, vnode) {
-    el.clickOutsideEvent = function(event: MouseEvent) {
+    el.clickOutsideEvent = function(event: MouseEvent | TouchEvent) {
       // Check if the click was outside the element and its children
       if (!(el === event.target || el.contains(event.target as Node))) {
         // Call the method provided in the directive's value
-        const callback = binding.value as (event: MouseEvent) => void;
+        const callback = binding.value as (
+          event: MouseEvent | TouchEvent
+        ) => void;
         if (typeof callback === "function") {
           callback(event);
         }
       }
     };
     document.body.addEventListener("click", el.clickOutsideEvent);
+    document.body.addEventListener("touchstart", el.clickOutsideEvent);
   },
   unbind(el: ClickOutsideElement) {
     document.body.removeEventListener("click", el.clickOutsideEvent!);
+    document.body.removeEventListener("touchstart", el.clickOutsideEvent!);
   },
 };
 
