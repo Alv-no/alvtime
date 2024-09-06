00c27aee894df107caf18bf5319686b73d094fcc
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2024-09-06 14:02:41 +0200

    extract core navigation bar

--- packages/app/lib/features/profilescreen/profilescreen.dart
+++ packages/app/lib/features/profilescreen/profilescreen.dart
@@ -1,6 +1,8 @@
 import 'package:alv_time_app/const.dart';
-import 'package:alv_time_app/features/homescreen/widgets/gradient/gradient.dart';
+import 'package:alv_time_app/core/widgets/core_navigation_bar/core_navigation_bar.dart';
 import 'package:flutter/material.dart';
+
+import '../../core/widgets/gradient/core_gradient.dart';
 
 class ProfileScreenWidget extends StatelessWidget {
   const ProfileScreenWidget({super.key});
@@ -8,7 +10,8 @@ class ProfileScreenWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TimebasedGradientBackground(
+      extendBodyBehindAppBar: true,
+      body: CoreTimebasedGradientBackground(
           child: const Text("Profil",
               style: TextStyle(color: Colors.white, fontSize: 40))),
       appBar: AppBar(
@@ -16,16 +19,7 @@ class ProfileScreenWidget extends StatelessWidget {
         appName,
         style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
       )),
-      bottomNavigationBar: NavigationBar(
-          backgroundColor: Colors.transparent,
-          elevation: 30,
-          destinations: const [
-            NavigationDestination(
-                icon: Icon(Icons.home_filled), label: 'Oversikt'),
-            NavigationDestination(
-                icon: Icon(Icons.bar_chart), label: 'Statistikk'),
-            NavigationDestination(icon: Icon(Icons.person), label: 'Profil'),
-          ]),
+      bottomNavigationBar: const CoreNavigationBar()
     );
   }
 }
