a72c3be5d69c5fb5defe38441599367a7dd71429
jtb <jan@boee.org>
2024-09-06 09:56:50 +0200

    Add IsLocked column to PaidOvertime table
    Introduced a new boolean column 'IsLocked' in the PaidOvertime table to indicate if the record is locked. Updated the Entity Framework migrations and database model snapshot to reflect this change. Minor formatting adjustments in OvertimeController.

--- packages/api/AlvTime.Persistence/DatabaseModels/PaidOvertime.cs
+++ packages/api/AlvTime.Persistence/DatabaseModels/PaidOvertime.cs
@@ -7,11 +7,11 @@ namespace AlvTime.Persistence.DatabaseModels
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
+        public bool IsLocked { get; set; }
         public int User { get; set; }
         public decimal HoursBeforeCompRate { get; set; }
         public decimal HoursAfterCompRate { get; set; }
         public decimal CompensationRate { get; set; }
-
         public virtual User UserNavigation { get; set; }
     }
 }
