ebecbec949d5ceb406bd828f00e56e2736b9237b
juliekongsten <juliekongsten@gmail.com>
2024-09-06 17:19:58 +0200

    added carousel component

---
+++ packages/app/lib/features/homescreen/carousel.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class Carousel extends StatelessWidget {
+  final CarouselSliderController _controller = CarouselSliderController();
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      carouselController: _controller,
+      options: CarouselOptions(
+        height: 400.0,
+        autoPlay: false,
+        enlargeCenterPage: true,
+        autoPlayInterval: const Duration(seconds: 3),
+        autoPlayAnimationDuration: const Duration(milliseconds: 800),
+        autoPlayCurve: Curves.fastOutSlowIn,
+        scrollDirection: Axis.horizontal,
+      ),
+      items: ["Kunder", "Internt", "Fravær"].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Center(
+                child: Text(
+                  '$i',
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+}
--- packages/app/lib/features/homescreen/homescreen.dart
+++ packages/app/lib/features/homescreen/homescreen.dart
@@ -1,6 +1,7 @@
-import 'package:alv_time_app/const.dart';
-import 'package:alv_time_app/features/homescreen/widgets/gradient/gradient.dart';
 import 'package:flutter/material.dart';
+import 'package:alv_time_app/features/homescreen/carousel.dart';
+import 'package:alv_time_app/features/homescreen/widgets/gradient/gradient.dart';
+import 'package:alv_time_app/const.dart';
 
 class HomeScreenWidget extends StatelessWidget {
   const HomeScreenWidget({super.key});
@@ -9,27 +10,40 @@ class HomeScreenWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Text(
+          appName,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
       body: TimebasedGradientBackground(
-        child: const Text(
-          'Faktureringsgrad',
-          style: TextStyle(color: Colors.white, fontSize: 40),
+        body: Carousel(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Faktureringsgrad',
+                style: TextStyle(color: Colors.white, fontSize: 40),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
-      appBar: AppBar(
-          title: const Text(
-        appName,
-        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-      )),
       bottomNavigationBar: NavigationBar(
-          backgroundColor: Colors.transparent,
-          elevation: 30,
-          destinations: const [
-            NavigationDestination(
-                icon: Icon(Icons.home_filled), label: 'Oversikt'),
-            NavigationDestination(
-                icon: Icon(Icons.bar_chart), label: 'Statistikk'),
-            NavigationDestination(icon: Icon(Icons.person), label: 'Profil'),
-          ]),
+        backgroundColor: Colors.transparent,
+        elevation: 30,
+        destinations: const [
+          NavigationDestination(
+              icon: Icon(Icons.home_filled), label: 'Oversikt'),
+          NavigationDestination(
+              icon: Icon(Icons.bar_chart), label: 'Statistikk'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profil'),
+        ],
+      ),
     );
   }
 }
