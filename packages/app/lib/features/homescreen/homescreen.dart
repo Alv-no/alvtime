206eaac749756493ef6c9a2f047b86b310c6754e
juliekongsten <juliekongsten@gmail.com>
2024-09-06 17:19:58 +0200

    Carousel improvements Added carousel component - align carousel bottom center - Adjust carousel card size

--- packages/app/lib/features/homescreen/homescreen.dart
+++ packages/app/lib/features/homescreen/homescreen.dart
@@ -1,7 +1,6 @@
-import 'package:alv_time_app/const.dart';
-import 'package:alv_time_app/core/widgets/core_navigation_bar/core_navigation_bar.dart';
-
 import 'package:flutter/material.dart';
+import 'package:alv_time_app/features/homescreen/carousel.dart'; // Correct import for Carousel widget
+import 'package:alv_time_app/const.dart';
 
 import '../../core/widgets/gradient/core_gradient.dart';
 
@@ -12,18 +11,38 @@ class HomeScreenWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      body: CoreTimebasedGradientBackground(
-        child: const Text(
-          'Faktureringsgrad',
-          style: TextStyle(color: Colors.white, fontSize: 40),
-        ),
-      ),
       appBar: AppBar(
-          title: const Text(
-        appName,
-        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-      )),
-      bottomNavigationBar: const CoreNavigationBar()
+        title: const Text(
+          appName,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Stack(
+        children: [
+          CoreTimebasedGradientBackground(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Faktureringsgrad',
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment:
+                Alignment.bottomCenter, // Align Carousel at the bottom center
+            child: Carousel(), // Carousel widget
+          ),
+        ],
+      ),
+      bottomNavigationBar: CoreNavigationBar(),
     );
   }
 }
