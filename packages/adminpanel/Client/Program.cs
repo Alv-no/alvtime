ed174e180edfc7cb932409139e307950f4c10e55
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2024-09-18 18:57:42 +0200

    add support for localization and removal of hard coded content strings

--- packages/adminpanel/Client/Program.cs
+++ packages/adminpanel/Client/Program.cs
@@ -10,6 +10,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddLocalization();
+
 builder.Services.AddTransient<CustomAuthorizationMessageHandler>();
 
 builder.Services.AddHttpClient("Alvtime.API", (sp, client) =>
