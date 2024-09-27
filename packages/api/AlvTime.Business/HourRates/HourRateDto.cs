b7d4f263459b602368cfd035c46afdd600a861ab
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2024-09-27 08:27:21 +0200

    add task id and task name to hour rate model

--- packages/api/AlvTime.Business/HourRates/HourRateDto.cs
+++ packages/api/AlvTime.Business/HourRates/HourRateDto.cs
@@ -7,4 +7,6 @@ public class HourRateDto
     public int Id { get; set; }
     public DateTime FromDate { get; set; }
     public decimal Rate { get; set; }
+    public int TaskId { get; set; }
+    public string TaskName { get; set; }
 }
