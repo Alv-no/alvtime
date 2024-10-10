38defc5e55130a7df53bd90bff925757ef51cd79
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2024-10-10 23:39:06 +0200

    rename response to same convention as others

--- packages/api/AlvTimeWebApi/Responses/Admin/CustomerAdminResponse.cs
+++ packages/api/AlvTimeWebApi/Responses/Admin/CustomerAdminResponse.cs
@@ -2,7 +2,7 @@
 
 namespace AlvTimeWebApi.Responses.Admin;
 
-public class CustomerDetailedResponse
+public class CustomerAdminResponse
 {
     public int? Id { get; set; }
     public string Name { get; set; }
