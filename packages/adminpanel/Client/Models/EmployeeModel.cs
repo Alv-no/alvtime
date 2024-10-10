2e9234d265d88457a6a22b61fd97c37d198b0857
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2024-10-10 18:29:29 +0200

    add employee details page

--- packages/adminpanel/Client/Models/EmployeeModel.cs
+++ packages/adminpanel/Client/Models/EmployeeModel.cs
@@ -12,10 +12,30 @@ public class EmployeeModel
     public IList<EmployeeEmploymentRateModel>? EmploymentRates { get; set; }
 }
 
-public class EmployeeEmploymentRateModel
+public class EmployeeEmploymentRateModel : IEquatable<EmployeeEmploymentRateModel>
 {
     public int Id { get; set; }
     public decimal RatePercentage { get; set; }
     public DateTime? FromDateInclusive { get; set; }
     public DateTime? ToDateInclusive { get; set; }
+    
+    public bool Equals(EmployeeEmploymentRateModel? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Id == other.Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((EmployeeEmploymentRateModel)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id;
+    }
 }
