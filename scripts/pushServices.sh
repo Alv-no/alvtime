9b9a07c00af93bda8478c6b3db691937b780c9b8
Pål Bøckmann <plbockmann@gmail.com>
2024-11-12 10:30:45 +0100

    Prod stage for api pipeline

--- scripts/pushServices.sh
+++ scripts/pushServices.sh
@@ -3,5 +3,4 @@
 set -e
 
 docker push "$CONTAINER_REGISTRY.azurecr.io/alvtime-frontend:$SHORT_HASH"
-docker push "$CONTAINER_REGISTRY.azurecr.io/alvtime-web-api:$SHORT_HASH"
 docker push "$CONTAINER_REGISTRY.azurecr.io/alvtime-slack-app:$SHORT_HASH"
