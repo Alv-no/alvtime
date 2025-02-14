b68ee98c2380cb9711eaa5943043a8a1a1275199
Pål Bøckmann <plbockmann@gmail.com>
2025-02-14 13:00:26 +0100

    Slette AzDO og k8s stuff

--- scripts/pushServices.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker push "$CONTAINER_REGISTRY.azurecr.io/alvtime-frontend:$SHORT_HASH"
-docker push "$CONTAINER_REGISTRY.azurecr.io/alvtime-slack-app:$SHORT_HASH"
