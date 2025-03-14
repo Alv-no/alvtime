0567c196fc31dc566476538544f6ede7efa403ff
Pål Bøckmann <plbockmann@gmail.com>
2025-03-14 12:52:18 +0100

    Startet å oppgradere til .NET 9

--- packages/api/AlvTimeWebApi/Startup.cs
+++ packages/api/AlvTimeWebApi/Startup.cs
@@ -13,82 +13,79 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace AlvTimeWebApi
+namespace AlvTimeWebApi;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IHostEnvironment env)
     {
-        public Startup(IHostEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables()
-                .AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables()
+            .AddUserSecrets(Assembly.GetExecutingAssembly(), true);
 
-            Configuration = builder.Build();
+        Configuration = builder.Build();
+    }
+
+    private IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAlvtimeServices(Configuration);
+        services.AddDbContext<AlvTime_dbContext>(
+            options => options.UseSqlServer(Configuration.GetConnectionString("AlvTime_db")),
+            contextLifetime: ServiceLifetime.Scoped, optionsLifetime: ServiceLifetime.Scoped);
+        services.AddMvc();
+        services.AddAlvtimeAuthentication(Configuration);
+        services.Configure<TimeEntryOptions>(Configuration.GetSection("TimeEntryOptions"));
+        services.AddAlvtimeAuthorization();
+        services.AddSwaggerExtensions(Configuration);
+        services.AddRazorPages();
+        services.AddAlvtimeCorsPolicys(Configuration);
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (!env.IsDevelopment())
+        {
+            app.UseHsts(); // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         }
 
-        private IConfiguration Configuration { get; }
+        app.UseStaticFiles();
+        app.UseErrorHandling(env);
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
         {
-            services.AddAlvtimeServices(Configuration);
-            services.AddDbContext<AlvTime_dbContext>(
-                options => options.UseSqlServer(Configuration.GetConnectionString("AlvTime_db")),
-                contextLifetime: ServiceLifetime.Scoped, optionsLifetime: ServiceLifetime.Scoped);
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-            services.AddAlvtimeAuthentication(Configuration);
-            services.Configure<TimeEntryOptions>(Configuration.GetSection("TimeEntryOptions"));
-            services.AddAlvtimeAuthorization();
-            services.AddSwaggerExtensions(Configuration);
-            services.AddRazorPages();
-            services.AddAlvtimeCorsPolicys(Configuration);
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Alvtime API v1");
+            c.OAuthClientId(Configuration["AzureAd:ClientId"]);
+            c.OAuthUsePkce();
+            c.OAuthScopeSeparator(" ");
+        });
+
+        app.UseRouting();
+
+        if (env.IsDevelopment())
+        {
+            app.UseCors(CorsExtensions.DevCorsPolicyName);
+        }
+        else
+        {
+            app.UseCors();
+            app.UseHttpsRedirection();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
         {
-            if (!env.IsDevelopment())
-            {
-                app.UseHsts(); // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            }
-
-            app.UseStaticFiles();
-            app.UseErrorHandling(env);
-
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Alvtime API v1");
-                c.OAuthClientId(Configuration["AzureAd:ClientId"]);
-                c.OAuthUsePkce();
-                c.OAuthScopeSeparator(" ");
-            });
-
-            app.UseRouting();
-
-            if (env.IsDevelopment())
-            {
-                app.UseCors(CorsExtensions.DevCorsPolicyName);
-            }
-            else
-            {
-                app.UseCors();
-                app.UseHttpsRedirection();
-            }
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
-        }
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            endpoints.MapRazorPages();
+        });
     }
 }
