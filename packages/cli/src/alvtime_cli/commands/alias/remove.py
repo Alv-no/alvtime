640ceb2a5410d288aef9a0485f404ddde48ebbc6
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2025-05-02 14:53:32 +0200

    Add alias delete

--- packages/cli/src/alvtime_cli/commands/alias/remove.py
+++ packages/cli/src/alvtime_cli/commands/alias/remove.py
@@ -1,9 +1,20 @@
 import click
 
+from alvtime_cli import config
+
+
+def _remove_alias(name: str):
+    aliases = config.get(config.Keys.task_aliases, {})
+
+    if name not in aliases.values():
+        raise click.ClickException(f"No alias found with name '{name}'")
+    
+    aliases = { k:v for k,v in aliases.items() if v != name }
+    config.set(config.Keys.task_aliases, aliases)
 
 @click.command()
 @click.argument("name", type=str)
-def remove():
+def remove(name: str):
     """
     Removes a task alias
 
@@ -11,4 +22,4 @@ def remove():
 
     $ alvtime alias remove alvops
     """
-    pass
+    _remove_alias(name)
