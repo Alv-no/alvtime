eb17b51bfe52c6e6703debf26e3ebf1ebcefe97e
Anders Norman <anders@alv.no>
2025-05-11 12:40:56 +0200

    Stop leaking click into LocalService

--- packages/cli/src/alvtime_cli/commands/start.py
+++ packages/cli/src/alvtime_cli/commands/start.py
@@ -3,7 +3,7 @@ from typing import cast
 import click
 
 from alvtime_cli.utils import AliasParam
-from alvtime_cli.local_service import LocalService
+from alvtime_cli.local_service import LocalService, TaskAlreadyStartedError
 from alvtime_cli import model
 from alvtime_cli.utils import style_time_entry
 
@@ -16,13 +16,12 @@ from alvtime_cli.utils import style_time_entry
 def start(ctx, alias: model.TaskAlias, at: datetime = None, comment: str = None):
     service = cast(LocalService, ctx.obj)
 
-    current_entry = service.current_entry()
-    if current_entry:
+    try:
+        time_entry = service.start(
+            task_id=alias.task_id,
+            at=at,
+            comment=comment)
+    except TaskAlreadyStartedError:
         raise click.exceptions.ClickException("Project already running")
-
-    time_entry = service.start(
-        task_id=alias.task_id,
-        at=at,
-        comment=comment)
 
     click.echo(style_time_entry(time_entry))
