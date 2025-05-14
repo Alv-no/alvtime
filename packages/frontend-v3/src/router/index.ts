3b21306f105785fe643723a2a4c18f1d27cbd2d6
Boye Svensson <boyevnsvensson@gmail.com>
2025-05-14 10:19:30 +0200

    Try to fix some pathing and linting stuff

--- packages/frontend-v3/src/router/index.ts
+++ packages/frontend-v3/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const routes = [
 	{
--- packages/frontend-v3/vite.config.ts
+++ packages/frontend-v3/vite.config.ts
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+	resolve: {
+    	alias: {
+      		'@': '/src',
+			'~': '/src',
+		}
+	}
+});
