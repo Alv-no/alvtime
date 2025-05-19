162e40bebbeeff82c2f5adc43e2c9c1bada9c792
Boye Svensson <boyevnsvensson@gmail.com>
2025-05-19 13:00:52 +0200

    Updates application styling and fonts
    Improves the application's visual appearance by switching to SCSS for styling and incorporating custom fonts from Google Fonts.
    
    This change introduces a more modern and visually appealing design, enhancing the user experience.

--- packages/frontend-v3/src/main.ts
+++ packages/frontend-v3/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import "./style.css";
+import "./style.scss";
 import App from "./App.vue";
 import { router } from "./router";
 import { createPinia } from "pinia";
