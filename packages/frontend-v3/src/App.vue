b115073415ff0ff10fe340534c0965df973337a5
Boye Svensson <boyevnsvensson@gmail.com>
2025-05-19 13:01:15 +0200

    Sets up basic application layout
    Creates a basic application layout with a navigation bar and a router view.
    
    The navigation bar component provides a basic structure for site navigation.
    Removes the previous authentication-related code from the App.vue file.

--- packages/frontend-v3/src/App.vue
+++ packages/frontend-v3/src/App.vue
@@ -1,24 +1,10 @@
 <template>
-  <button @click="loginRedirect">
-    Logg inn
-  </button>
-  <p v-if="isAuthenticated">
-    Du er logget inn!
-  </p>
-  <RouterView :key="$route.path" />
+	<NavigationBar />
+	<RouterView :key="$route.path" />
 </template>
 
 <script setup lang="ts">
-import { useIsAuthenticated } from './composables/useIsAuthenticated';
-import { useMsal } from './composables/useMsal';
-import { loginRequest } from "./authConfig";
-
-const isAuthenticated = useIsAuthenticated();
-const { instance } = useMsal();
-    
-const loginRedirect = () => {
-  instance.loginRedirect(loginRequest);
-}
+import NavigationBar from './components/NavigationBar.vue';
 </script>
 
 <style lang="scss" scoped>
---
+++ packages/frontend-v3/src/components/NavigationBar.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="navigation-bar">
+		<div class="navigation-container">
+			<p>Alvtime</p>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.navigation-bar {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 60px;
+	background-color: rgb(36, 63, 77);
+	color: rgb(248, 241, 227);
+
+	.navigation-container {
+		width: 1200px;
+		padding: 0 20px;
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+
+		a {
+			color: white;
+			text-decoration: none;
+			margin-right: 20px;
+
+			&:hover {
+				text-decoration: underline;
+			}
+		}
+	}
+}
+</style>
