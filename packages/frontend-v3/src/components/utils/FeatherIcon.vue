04f49b0debfa02de11e8de03db300b1df224f06f
Boye Svensson <boyevnsvensson@gmail.com>
2025-05-21 17:27:49 +0200

    Adds Feather Icons component
    Introduces a new `FeatherIcon` component for rendering Feather icons in the application.
    
    This commit adds the `feather-icons` and `vue-feather-icons` dependencies, along with the corresponding type definitions.
    The `FeatherIcon.vue` component encapsulates the logic for displaying icons based on their name and size, simplifying their usage throughout the application.
    The component also includes basic styling to vertically align the icon with the surrounding text.

---
+++ packages/frontend-v3/src/components/utils/FeatherIcon.vue
@@ -0,0 +1,35 @@
+<template>
+	<span v-html="svgIcon" />
+</template>
+
+<script setup lang="ts">
+import feather from "feather-icons";
+import { computed } from "vue";
+
+const { name, size } = defineProps({
+	name: {
+		type: String,
+		required: true,
+	},
+	size: {
+		type: Number,
+		default: 16,
+	},
+})
+
+const svgIcon = computed(() => {
+	const icon = feather.icons[name as keyof typeof feather.icons];
+	if (icon) {
+		return icon.toSvg({ width: size, height: size, class: "feather-icon" });
+	}
+
+	return "";
+});
+</script>
+
+<style lang="scss">
+	.feather-icon {
+		position: relative;
+		top: 3px;
+	}
+</style>
