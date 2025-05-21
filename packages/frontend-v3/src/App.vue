7682dc256763e4768c8911fc9dec751d7c1eefe1
Boye Svensson <boyevnsvensson@gmail.com>
2025-05-21 14:49:03 +0200

    Adds base layout for the application
    Implements the base layout with a blue bar and content container for the application.
    
    It introduces a wrapper with a blue bar for visual appeal and a content container for the main content with a defined maximum width, padding, and background color.
    
    The background color is moved to the html and body to cover the whole screen.

--- packages/frontend-v3/src/App.vue
+++ packages/frontend-v3/src/App.vue
@@ -1,6 +1,11 @@
 <template>
 	<NavigationBar />
-	<RouterView :key="$route.path" />
+	<div class="wrapper">
+		<div class="blue-bar" />
+		<div class="content-container">
+			<RouterView :key="$route.path" />
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -8,5 +13,30 @@ import NavigationBar from "./components/NavigationBar.vue";
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+	position: relative;
+
+	.blue-bar {
+		background-color: $primary-color;
+		z-index: 4;
+		position: absolute;
+		height: 400px;
+		width: 100%;
+		top: 0;
+		left: 0;
+	}
+
+	.content-container {
+		position: relative;
+		max-width: 1200px;
+		margin: 0 auto;
+		padding: 16px;
+		z-index: 6;
+		background-color: $background-color;
+		height: 100%;
+		border-radius: 25px;
+		min-height: 70vh;
+	}
+}
 
 </style>
