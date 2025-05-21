f190d77b2173fd2f81a12a274d96787215a34b18
Boye Svensson <boyevnsvensson@gmail.com>
2025-05-21 17:28:05 +0200

    Adds status bar component
    Adds a status bar component to display relevant information, such as data and calendar information.
    
    The status bar enhances the user interface by providing quick access to key metrics.

---
+++ packages/frontend-v3/src/components/StatusBar.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="status-bar-container">
+		<div class="column-box">
+			<div class="content-box">
+				<FeatherIcon
+					name="calendar"
+					:size="18"
+				/> 22,5/37,5
+			</div>
+			<div class="content-box">
+				<FeatherIcon
+					name="database"
+					:size="18"
+				/> 4
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import FeatherIcon from "@/components/utils/FeatherIcon.vue";
+</script>
+
+<style scoped lang="scss">
+	.status-bar-container {
+		background-color: rgb(157, 172, 134);
+		border-radius: 15px;
+		width: 100%;
+		display: flex;
+		justify-content: flex-start;
+		align-items: flex-start;
+
+		.column-box {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: flex-start;
+			padding: 16px;
+			gap: 8px;
+		}
+
+		.content-box {
+			border-radius: 10px;
+			padding: 12px;
+			background-color: rgb(206 214 194);
+		}
+	}
+</style>
