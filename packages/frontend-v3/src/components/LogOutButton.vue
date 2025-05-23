27ccb5bc46623b224ada9b0af56f5b341f3b5594
Boye Svensson <boyevnsvensson@gmail.com>
2025-05-23 14:18:46 +0200

    Updates logout button style and adds icon
    Enhances the logout button with updated styling and includes a logout icon for improved user experience.
    The logout button now has a distinct appearance with a new background color, text color, border radius and padding.

--- packages/frontend-v3/src/components/LogOutButton.vue
+++ packages/frontend-v3/src/components/LogOutButton.vue
@@ -1,31 +1,33 @@
 <template>
 	<button @click="logOut">
-		Logg ut
+		Logg ut <FeatherIcon name="log-out" />
 	</button>
 </template>
 
 <script setup lang="ts">
 import { useMsal } from "@/composables/useMsal";
+import FeatherIcon from "./utils/FeatherIcon.vue";
 
 const { instance } = useMsal();
 
 const logOut = async () => {
-	// Log out the user
 	await instance.logoutRedirect();
 };
 </script>
 
 <style scoped lang="scss">
 button {
-	background-color: transparent;
-	color: $background-color;
+	background-color: $secondary-color;
+	color: $primary-color;
+	border-radius: 25px;
 	border: none;
-	padding: 10px 10px;
+	padding: 9px 16px 12px 16px;
 	cursor: pointer;
 	font-size: 14px;
+	font-weight: 600;
 
 	&:hover {
-		background-color: $background-color;
+		background-color: $accent-color;
 		color: $primary-color;
 	}
 }
