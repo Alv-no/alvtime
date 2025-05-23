7326297b12eb0f35c29d7ae971a8888376264f78
Boye Svensson <boyevnsvensson@gmail.com>
2025-05-23 14:20:01 +0200

    Adds TimeTracker component to HomeView
    Implements the TimeTracker component in the HomeView
    to provide users with a time tracking interface.
    
    Adds basic styling for spacing.

--- packages/frontend-v3/src/views/HomeView.vue
+++ packages/frontend-v3/src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
 	<StatusBar />
+	<TimeTracker class="margin-top" />
 </template>
 
 <script setup lang="ts">
+import StatusBar from "@/components/StatusBar.vue";
+import TimeTracker from "@/components/TimeTracker/TimeTracker.vue";
 
 </script>
 
 <style scoped lang="scss">
-
+.margin-top {
+	margin-top: 16px;
+}
 </style>
