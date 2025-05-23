ecc932b75768d38d0ac57d12c7a05459ec143ddf
Boye Svensson <boyevnsvensson@gmail.com>
2025-05-23 14:19:27 +0200

    Adds TimeTracker component
    Adds the TimeTracker component to display the week number and includes a CustomerExpandable component.
    
    This sets up the basic structure for time tracking functionality.

---
+++ packages/frontend-v3/src/components/TimeTracker/TimeTracker.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="time-tracker-container">
+		<div class="time-tracker-header">
+			<div class="week-number-container">
+				UKE 21
+			</div>
+		</div>
+		<CustomerExpandable />
+	</div>
+</template>
+
+<script setup lang="ts">
+import CustomerExpandable from "./CustomerExpandable.vue";
+</script>
+
+<style scoped lang="scss">
+.time-tracker-header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+
+.week-number-container {
+	background-color: rgb(230, 192, 141);
+	border-radius: 10px;
+	padding: 12px 12px 9px;
+	font-weight: 600;
+	font-size: 14px;
+}
+</style>
