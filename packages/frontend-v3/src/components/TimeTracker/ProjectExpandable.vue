5dec1e414633e26541afd044cd2ae6a41ce55991
Boye Svensson <boyevnsvensson@gmail.com>
2025-05-28 14:28:42 +0200

    Enables expandable project view
    Implements an expandable section for projects, allowing users to view associated tasks within each project.
    
    Clicking a project expands or collapses the task list.

--- packages/frontend-v3/src/components/TimeTracker/ProjectExpandable.vue
+++ packages/frontend-v3/src/components/TimeTracker/ProjectExpandable.vue
@@ -1,28 +1,37 @@
 <template>
-	<div class="project-container">
-		<div class="project-info-wrapper">
-			<p class="project-name">
-				{{ project.name }}
-			</p>
-			<p class="project-project">
-				{{ project.customer.name}}
-			</p>
-		</div>
-		<div class="project-status-wrapper">
-			<div class="project-stats">
-				Denne uken: 22,5t | Denne måneden: 145,5t
+	<div class="project-container" @click="toggleExpand">
+		<div class="project-container-header">
+			<div class="project-info-wrapper">
+				<p class="project-name">
+					{{ project.name }}
+				</p>
+				<p class="project-project">
+					{{ project.customer.name}}
+				</p>
 			</div>
-			<FeatherIcon
-				name="chevron-down"
-				:size="36"
-			/>
+			<div class="project-status-wrapper">
+				<div class="project-stats">
+					Denne uken: 22,5t | Denne måneden: 145,5t
+				</div>
+				<FeatherIcon
+					name="chevron-down"
+					:size="36"
+				/>
+			</div>
+		</div>
+		<div class="project-container-expandable" :class="{ 'visible': isExpanded }">
+			<div class="project-container-content">
+				<div v-for="task in project.tasks" :key="task.id">
+					{{ task.name }}
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import FeatherIcon from "../utils/FeatherIcon.vue";
-import { defineProps } from "vue";
+import { defineProps, ref } from "vue";
 
 interface Customer {
 	name: string;
@@ -33,6 +42,12 @@ interface Project {
 	customer: Customer;
 }
 
+const isExpanded = ref(false);
+
+const toggleExpand = () => {
+	isExpanded.value = !isExpanded.value;
+};
+
 const { project } = defineProps<{
 	project: Project;
 }>();
@@ -40,13 +55,14 @@ const { project } = defineProps<{
 
 <style scoped lang="scss">
 .project-container {
+	border: 1px solid rgb(206, 214, 194, 0.5);
+	border-radius: 10px;
+}
+.project-container-header {
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
 	padding: 4px 16px;
-	margin-top: 16px;
-	border: 1px solid rgb(206, 214, 194, 0.5);
-	border-radius: 10px;
 	min-height: 70px;
 
 	.project-info-wrapper {
@@ -83,5 +99,22 @@ const { project } = defineProps<{
 			padding: 12px 12px 9px;
 		}
 	}
+}
+.project-container-expandable {
+	display: grid;
+	grid-template-rows: 0fr;
+	transition: grid-template-rows 200ms ease-in-out;
+
+	&.visible {
+		grid-template-rows: 1fr;
+		margin-bottom: 16px;
+	}
+}
+.project-container-content {
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+	overflow: hidden;
+	padding: 0 16px;
 }
 </style>
