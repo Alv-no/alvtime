1e1a2a1fb7485bd5a80654ceb25cd0d4233e8e2e
Pål Bøckmann <pal@alv.no>
2025-06-04 15:12:45 +0200

    Bruker DefaultAzureCredentials i miljøer

--- packages/api/AlvTimeWebApi/Infrastructure/AzureExtensions.cs
+++ packages/api/AlvTimeWebApi/Infrastructure/AzureExtensions.cs
@@ -2,25 +2,33 @@
 using Azure.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Graph;
 
 namespace AlvTimeWebApi.Infrastructure;
 
 public static class AzureExtensions
 {
-    public static void AddMicrosoftGraphClient(this IServiceCollection services, IConfiguration configuration)
+    public static void AddMicrosoftGraphClient(this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
     {
         var authentication = new OAuthOptions();
         configuration.Bind("AzureAd", authentication);
 
         var scopes = new[] { "https://graph.microsoft.com/.default" };
 
-        var tenantId = authentication.TenantId;
-        var clientId = authentication.ClientId;
-        var clientSecret = authentication.GraphClientSecret;
+        if (environment.IsDevelopment())
+        {
+            var tenantId = authentication.TenantId;
+            var clientId = authentication.ClientId;
+            var clientSecret = authentication.GraphClientSecret;
 
-        var options = new ClientSecretCredential(tenantId, clientId, clientSecret);
-        var graphClient = new GraphServiceClient(options, scopes);
-        services.AddSingleton(graphClient);
+            var options = new ClientSecretCredential(tenantId, clientId, clientSecret);
+            services.AddSingleton(new GraphServiceClient(options, scopes));
+        }
+        else
+        {
+            var credential = new DefaultAzureCredential();
+            services.AddSingleton(new GraphServiceClient(credential, scopes));
+        }
     }
 }
