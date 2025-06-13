1d4a439a2b77b67beb51836bcbf6f663560d3884
Boye Svensson <boyevnsvensson@gmail.com>
2025-06-13 10:05:23 +0200

    Adds TaskStrip component
    Creates a component to display task information and its associated week.
    
    This component includes the task name, compensation rate, and a child component to manage task details for a specific week.
    
    Styles the component to improve layout and visual appeal.

--- packages/frontend-v3/src/components/TimeTracker/TaskStrip.vue
+++ packages/frontend-v3/src/components/TimeTracker/TaskStrip.vue
@@ -1,20 +1,35 @@
 <template>
-	<div>{{ task.name }} - {{ compensationRateInPercentage }}</div>
+	<div class="task-strip">
+		<div class="task-description">
+			{{ task.name }} - {{ compensationRateInPercentage }}
+		</div>
+		<TaskWeek
+			:task="task"
+			:week="week"
+		/>
+	</div>
 </template>
 
 <script lang="ts" setup>
 import { defineProps, computed } from "vue";
 import { type Task } from "@/types/ProjectTypes";
+import TaskWeek from "./TaskWeek.vue";
 
-const { task } = defineProps<{
+const { task, week } = defineProps<{
   task: Task;
+  week: Date[];
 }>();
 
 const compensationRateInPercentage = computed(() => {
-  return `${task.compensationRate * 100}%`;
+  	return `${task.compensationRate * 100}%`;
 });
 </script>
 
 <style lang="scss" scoped>
-
+.task-strip {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	border-radius: 5px;
+}
 </style>
