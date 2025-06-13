070d4d1cafe8eb2155112db79c5490f955ecbc4d
Boye Svensson <boyevnsvensson@gmail.com>
2025-06-13 10:05:02 +0200

    Adds hour input component
    Creates a reusable hour input component for time tracking.
    
    This component allows users to input time values, handles locale-specific formatting (Norwegian), and updates the time entry in the store.
    
    The component includes basic styling and utilizes Vue's computed properties for two-way data binding.

---
+++ packages/frontend-v3/src/components/TimeTracker/HourInput.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+		<input
+			v-model="timeValue"
+			type="text"
+			class="form-control"
+		/>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import { type TimeEntry } from "@/types/TimeEntryTypes";
+import { useTimeEntriesStore } from "@/stores/timeEntriesStore";
+
+const timeEntriesStore = useTimeEntriesStore();
+
+const { timeEntry } = defineProps<{
+	timeEntry: TimeEntry;
+}>();
+
+const updateTimeEntry = (timeEntry: TimeEntry) => {
+	if (timeEntry.value) {
+		timeEntriesStore.updateTimeEntry(timeEntry);
+	}
+};
+
+const timeValue = computed({
+	get: () => timeEntry.value.toLocaleString('nb-NO'),
+	set: (newValue: string) => {
+		console.log("Setting new time value:", newValue);
+		const newNumber = parseFloat(newValue.replace(",", "."));
+		updateTimeEntry({ ...timeEntry, value: newNumber });
+	}
+});
+</script>
+
+<style lang="scss" scoped>
+input {
+	border-radius: 5px;
+	border: 1px solid #ccc;
+	background-color: rgb(250, 245, 235);
+	transition: border-color 0.3s ease;
+	padding: 8px;
+	width: 32px;
+	text-align: center;
+	font-size: 1rem;
+}
+</style>
