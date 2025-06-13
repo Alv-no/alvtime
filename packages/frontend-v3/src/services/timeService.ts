e49767a6d7bd0417b1f891c1031663f216e09c26
Boye Svensson <boyevnsvensson@gmail.com>
2025-06-13 10:06:06 +0200

    Introduces date management with Pinia stores
    Adds Pinia stores for managing the active date, weeks, and time entries.
    This includes creating stores for `dateStore` and `timeEntriesStore` to manage date state and time entry data, respectively.
    
    Refactors `timeService` to accept parameters as an object.
    
    Improves time entry handling with queueing and debouncing for efficient updates.

--- packages/frontend-v3/src/services/timeService.ts
+++ packages/frontend-v3/src/services/timeService.ts
@@ -3,8 +3,8 @@ import { formatDate } from "../utils/dateHelper";
 import { type TimeEntry } from "@/types/TimeEntryTypes";
 
 export default {
-	getTimeEntries: async (fromDateInclusive: Date, toDateInclusive: Date) => await api.get(
-		`/api/user/TimeEntries?fromDateInclusive=${formatDate(fromDateInclusive)}&toDateInclusive=${formatDate(toDateInclusive)}`
+	getTimeEntries: async (params: {fromDateInclusive: Date, toDateInclusive: Date}) => await api.get(
+		`/api/user/TimeEntries?fromDateInclusive=${formatDate(params.fromDateInclusive)}&toDateInclusive=${formatDate(params.toDateInclusive)}`
 	),
 	updateTimeEntries: async (timeEntries: TimeEntry[]) => await api.post(
 		"/api/user/TimeEntries", timeEntries
