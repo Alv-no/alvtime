50b67908d21de1be015b7dc43a7e3e43beac9f0d
Boye Svensson <boyevnsvensson@gmail.com>
2025-06-16 14:03:25 +0200

    Delays TimeTracker load until data is fetched
    Ensures that the TimeTracker component only renders after the necessary data (active date and tasks) has been fetched.
    
    This prevents potential issues or errors that might arise from the component attempting to access data before it's available.

--- packages/frontend-v3/src/views/HomeView.vue
+++ packages/frontend-v3/src/views/HomeView.vue
@@ -1,12 +1,24 @@
 <template>
 	<StatusBar />
-	<TimeTracker class="margin-top" />
+	<TimeTracker v-if="!loading" class="margin-top" />
 </template>
 
 <script setup lang="ts">
 import StatusBar from "@/components/StatusBar.vue";
 import TimeTracker from "@/components/TimeTracker/TimeTracker.vue";
+import { useTaskStore } from "@/stores/taskStore";
+import { useDateStore } from "@/stores/dateStore";
+import { ref, onMounted } from "vue";
 
+const loading = ref(true);
+
+onMounted(async () => {
+	const taskStore = useTaskStore();
+	const dateStore = useDateStore();
+	await dateStore.setActiveDate(new Date());
+	await taskStore.getTasks();
+	loading.value = false;
+});
 </script>
 
 <style scoped lang="scss">
