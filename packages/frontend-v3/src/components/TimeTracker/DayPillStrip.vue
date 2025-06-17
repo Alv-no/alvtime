d8a35eed0ae9741e95de650a7ba7052fa3868c85
Boye Svensson <boyevnsvensson@gmail.com>
2025-06-17 13:56:41 +0200

    Adds day pill component for time tracking
    Adds a DayPill component to display a date in a pill format.
    Also introduces a DayPillStrip component to display a row of DayPills for the week.
    The dayOfWeek utility function is added to format the day name.

---
+++ packages/frontend-v3/src/components/TimeTracker/DayPillStrip.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="day-pill-strip">
+		<DayPill
+			v-for="day in week"
+			:key="day.getMilliseconds()"
+			:day="day"
+		/>
+	</div>
+</template>
+
+<script setup lang="ts">
+import DayPill from "./DayPill.vue";
+
+const { week } = defineProps<{
+	week: Date[];
+}>();
+</script>
+
+<style scoped lang="scss">
+.day-pill-strip {
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+	gap: 1rem;
+}
+</style>
--- packages/frontend-v3/src/utils/dateHelper.ts
+++ packages/frontend-v3/src/utils/dateHelper.ts
@@ -2,6 +2,19 @@
  * Utility functions for date comparisons and formatting.
  */
 
+const dayOfWeek = (day: number) => {
+	switch (day) {
+	case 0: return "Søndag";
+	case 1: return "Mandag";
+	case 2: return "Tirsdag";
+	case 3: return "Onsdag";
+	case 4: return "Torsdag";
+	case 5: return "Fredag";
+	case 6: return "Lørdag";
+	default: throw new Error("Invalid day of the week");
+	}
+};
+
 const isBefore = (date: Date, compareDate: Date): boolean => {
 	return date.getTime() < compareDate.getTime();
 };
@@ -54,4 +67,5 @@ export {
 	todayIsBetween,
 	isSameDay,
 	formatDate,
+	dayOfWeek,
 };
