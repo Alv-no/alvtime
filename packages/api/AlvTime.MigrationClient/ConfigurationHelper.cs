c51b6673cd51ee2c9df6c42b8c939d8a1892aa55
Pål Bøckmann <pal@alv.no>
2025-06-19 11:31:00 +0200

    Flytter MigrationClient til eget prosjekt

---
+++ packages/api/AlvTime.MigrationClient/ConfigurationHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace AlvTime.MigrationClient;
+
+public static class ConfigurationHelper
+{
+    private static string? GetEnvironment() => Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    
+    public static void CommonConfigure<T>(this IConfigurationBuilder configurationBuilder) where T : class
+    {
+        CommonConfigure(configurationBuilder, typeof(T));
+    }
+    
+    private static void CommonConfigure(IConfigurationBuilder configurationBuilder, Type? userSecretsType)
+    {
+        var basePath = Directory.GetCurrentDirectory();
+        var environmentName = GetEnvironment();
+        configurationBuilder
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        if (!string.IsNullOrEmpty(environmentName))
+        {
+            configurationBuilder
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true);
+        }
+
+        if (userSecretsType != null)
+        {
+            configurationBuilder.AddUserSecrets(userSecretsType.Assembly, optional: true);
+        }
+
+        configurationBuilder.AddEnvironmentVariables();
+
+        var configuration = configurationBuilder.Build();
+
+        var keyVaultUrl = configuration["AzureKeyVault:Uri"];
+
+        if (!string.IsNullOrEmpty(keyVaultUrl))
+        {
+            configurationBuilder.AddAzureKeyVault(new Uri(keyVaultUrl), GetAzureCredential(), new DottableKeyVaultSecretManager());
+        }
+    }
+    
+    public static DefaultAzureCredential GetAzureCredential()
+    {
+        return new DefaultAzureCredential( new DefaultAzureCredentialOptions { ExcludeSharedTokenCacheCredential = true } );
+    }
+}
---
+++ packages/api/AlvTime.MigrationClient/DottableKeyVaultSecretManager.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+
+namespace AlvTime.MigrationClient;
+
+public class DottableKeyVaultSecretManager : KeyVaultSecretManager
+{
+    public override string GetKey(KeyVaultSecret secret)
+    {
+        var result = secret.Name.Replace("---", ".").Replace("--", ConfigurationPath.KeyDelimiter);
+
+        return result;
+    }
+}
