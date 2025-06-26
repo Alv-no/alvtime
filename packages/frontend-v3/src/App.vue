d3cca83090bb2d024bba43869ce06aa7ece5dc53
Boye Svensson <boyevnsvensson@gmail.com>
2025-06-26 15:07:39 +0200

    Adds padding to page content
    Improves the layout by adding padding to the bottom of the wrapper and adding margin to the content container.
    
    This change ensures that the content is spaced appropriately from the bottom of the screen.

--- packages/frontend-v3/src/App.vue
+++ packages/frontend-v3/src/App.vue
@@ -13,10 +13,12 @@ import NavigationBar from "./components/NavigationBar.vue";
 
 <style lang="scss" scoped>
 .wrapper {
+	padding-bottom: 32px;
 	.content-container {
 		position: relative;
 		max-width: 1200px;
 		margin: 0 auto;
+		margin-bottom: 20px;
 		padding: 16px;
 		background-color: $background-color;
 		height: 100%;
