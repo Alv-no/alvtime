b095d21dbca1f1975eeb84aa1644f1a022553bb8
Boye Svensson <boyevnsvensson@gmail.com>
2025-06-26 15:08:28 +0200

    Corrects holiday descriptions
    Updates the spelling and formatting of Norwegian holiday descriptions
    for improved readability.

--- packages/frontend-v3/src/utils/holidayHelper.ts
+++ packages/frontend-v3/src/utils/holidayHelper.ts
@@ -42,26 +42,26 @@ const createNorwegianHolidaysForYear = (yearNumber: number) => {
 
   	const norwegianHolidays = [
     	{ date: new Date(yearString + "-01-01"), description: "Første nyttårsdag" },
-		{ date: easterDateOffset(-7, easter), description: "Palmesøndag" },
-		{ date: easterDateOffset(-3, easter), description: "Skjærtorsdag" },
-		{ date: easterDateOffset(-2, easter), description: "Langfredag" },
+		{ date: easterDateOffset(-7, easter), description: "Palme-søndag" },
+		{ date: easterDateOffset(-3, easter), description: "Skjær-torsdag" },
+		{ date: easterDateOffset(-2, easter), description: "Lang-fredag" },
 		{ date: easter, description: "Første påskedag" },
 		{ date: easterDateOffset(1, easter), description: "Andre påskedag" },
 		{
 			date: new Date(yearString + "-05-01"),
 			description: "Arbeidernes dag",
 		},
-		{ date: new Date(yearString + "-05-17"), description: "Grunnlovsdag" },
+		{ date: new Date(yearString + "-05-17"), description: "Grunnlovs-dag" },
 		{
 			date: easterDateOffset(39, easter),
-			description: "Kristi himmelfartsdag",
+			description: "Kristi himmelfart",
 		},
 		{ date: easterDateOffset(49, easter), description: "Første pinsedag" },
 		{ date: easterDateOffset(50, easter), description: "Andre pinsedag" },
 		{ date: new Date(yearString + "-12-24"), description: "Juleaften" },
 		{ date: new Date(yearString + "-12-25"), description: "Første juledag" },
 		{ date: new Date(yearString + "-12-26"), description: "Andre juledag" },
-		{ date: new Date(yearString + "-12-31"), description: "Nyttårsaften" },
+		{ date: new Date(yearString + "-12-31"), description: "Nyttårs-aften" },
   	];
 
   	return norwegianHolidays;
