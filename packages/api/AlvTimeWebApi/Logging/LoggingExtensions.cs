22e951bb68d3824b2c5c930f01e66a94446d1aa5
Pål Bøckmann <pal@alv.no>
2025-09-04 07:51:50 +0200

    Bruker kun azure monitor i Azure

--- packages/api/AlvTimeWebApi/Logging/LoggingExtensions.cs
+++ packages/api/AlvTimeWebApi/Logging/LoggingExtensions.cs
@@ -1,12 +1,18 @@
 ﻿using Azure.Monitor.OpenTelemetry.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AlvTimeWebApi.Logging;
 
 public static class LoggingExtensions
 {
-    public static void ConfigureLogging(this IServiceCollection services)
+    public static void ConfigureLogging(this IServiceCollection services, IHostEnvironment environment)
     {
+        if (environment.IsDevelopment())
+        {
+            services.AddOpenTelemetry();
+            return;
+        }
         services.AddOpenTelemetry().UseAzureMonitor();
     }
 }
