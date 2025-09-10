568bbc4dd950d30bf9eec92d881fc5419c99e184
Pål Bøckmann <41721443+palbock@users.noreply.github.com>
2025-09-10 13:31:17 +0200

    Returns more details for admin customer endpoint (#1047)
    Co-authored-by: Pål Bøckmann <plbockmann@gmail.com>

--- packages/api/AlvTimeWebApi/Controllers/Admin/CustomerController.cs
+++ packages/api/AlvTimeWebApi/Controllers/Admin/CustomerController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AlvTime.Business.Projects;
 using AlvTimeWebApi.Controllers.Utils;
 using AlvTimeWebApi.ErrorHandling;
 using AlvTimeWebApi.Requests;
@@ -16,40 +15,31 @@ namespace AlvTimeWebApi.Controllers.Admin;
 [Route("api/admin")]
 [ApiController]
 [Authorize(Roles = "Admin")]
-public class CustomerController : ControllerBase
+public class CustomerController(CustomerService customerService) : ControllerBase
 {
-    private readonly CustomerService _customerService;
-    private readonly ProjectService _projectService;
-
-    public CustomerController(CustomerService customerService, ProjectService projectService)
-    {
-        _customerService = customerService;
-        _projectService = projectService;
-    }
-
     [HttpGet("Customers")]    
     public async Task<ActionResult<IEnumerable<CustomerAdminResponse>>> FetchCustomersAdmin()
     {
-        var customers = await _customerService.GetCustomersAdmin();
-        return Ok(customers.Select(c => c.MapToCustomerAdminResponse()));
+        var customers = await customerService.GetCustomersAdmin();
+        return Ok(customers.Select(c => c.MapToCustomerResponse()));
     }
 
     [HttpPost("Customers")]
     public async Task<ActionResult<CustomerResponse>> CreateNewCustomer([FromBody] CustomerUpsertRequest customerToBeCreated)
     {
-        var result = await _customerService.CreateCustomer(customerToBeCreated.MapToCustomerDto(null));
+        var result = await customerService.CreateCustomer(customerToBeCreated.MapToCustomerDto(null));
         return result.Match<ActionResult<CustomerResponse>>(
             customer => Ok(customer.MapToCustomerResponse()),
             errors => BadRequest(errors.ToValidationProblemDetails("Opprettelse av kunde feilet")));
     }
 
     [HttpGet("Customers/{customerId:int}")] 
-    public async Task<ActionResult<CustomerResponse>> GetCustomerById(int customerId)
+    public async Task<ActionResult<CustomerAdminResponse>> GetCustomerById(int customerId)
     {
-        var result = await _customerService.GetCustomerDetailedById(customerId);
-        return result.Match<ActionResult<CustomerResponse>>(
+        var result = await customerService.GetCustomerDetailedById(customerId);
+        return result.Match<ActionResult<CustomerAdminResponse>>(
             customer => customer != null 
-                ? Ok(CustomerMapper.MapToCustomerResponse(customer))
+                ? Ok(customer.MapToCustomerResponse())
                 : NotFound(),
             errors => BadRequest(errors.ToValidationProblemDetails("Henting av kunde feilet")));
     }
@@ -57,7 +47,7 @@ public class CustomerController : ControllerBase
     [HttpPut("Customers/{customerId:int}")]
     public async Task<ActionResult<CustomerResponse>> UpdateExistingCustomer([FromBody] CustomerUpsertRequest customerToBeUpdated, int customerId)
     {
-        var result = await _customerService.UpdateCustomer(customerToBeUpdated.MapToCustomerDto(customerId));
+        var result = await customerService.UpdateCustomer(customerToBeUpdated.MapToCustomerDto(customerId));
         return result.Match<ActionResult<CustomerResponse>>(
             customer => Ok(customer.MapToCustomerResponse()),
             errors => BadRequest(errors.ToValidationProblemDetails("Oppdatering av kunde feilet")));
