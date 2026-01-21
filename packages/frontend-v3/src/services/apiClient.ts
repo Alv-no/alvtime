42725dc26b6da4c17bac9dbfb1c2374e23809027
Pål Bøckmann <pal@alv.no>
2026-01-21 12:20:17 +0100

    Prøver loginpopup

--- packages/frontend-v3/src/services/apiClient.ts
+++ packages/frontend-v3/src/services/apiClient.ts
@@ -1,17 +1,28 @@
 import axios from "axios";
 import config from "@/config";
 import { msalInstance } from "@/authConfig";
+import { InteractionRequiredAuthError } from "@azure/msal-browser";
 
 const getAccessToken = async () => {
 	const accounts = msalInstance.getAllAccounts();
 	if (accounts.length > 0) {
 		const account = accounts[0];
-		const response = await msalInstance.acquireTokenSilent({
-			scopes: [config.ACCESS_SCOPE],
-			account: account,
-		});
+		try{
+			const response = await msalInstance.acquireTokenSilent({
+				scopes: [config.ACCESS_SCOPE],
+				account: account,
+			});
 
-		return response.accessToken;
+			return response.accessToken;
+
+		} catch (e) {
+			if (e instanceof InteractionRequiredAuthError) {
+				await msalInstance.loginPopup({
+					scopes: [config.ACCESS_SCOPE],
+					account: account,
+				});
+			}
+		}
 	} else {
 		throw new Error("No accounts found");
 	}
