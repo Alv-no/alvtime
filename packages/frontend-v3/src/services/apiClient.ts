4b508e9c249b4b7a6cd38b7e88522b62ba1192d6
Pål Bøckmann <41721443+palbock@users.noreply.github.com>
2026-01-26 10:46:49 +0100

    Lagrer ikke auth state in cookie (#1120)
    * Prøver loginpopup
    
    * Prøver noen andre triks
    
    * Prøver popup
    
    ---------
    
    Co-authored-by: Pål Bøckmann <pal@alv.no>

--- packages/frontend-v3/src/services/apiClient.ts
+++ packages/frontend-v3/src/services/apiClient.ts
@@ -1,30 +1,48 @@
 import axios from "axios";
 import config from "@/config";
 import { msalInstance } from "@/authConfig";
+import { InteractionRequiredAuthError } from "@azure/msal-browser";
 
 const getAccessToken = async () => {
 	const accounts = msalInstance.getAllAccounts();
 	if (accounts.length > 0) {
 		const account = accounts[0];
-		const response = await msalInstance.acquireTokenSilent({
-			scopes: [config.ACCESS_SCOPE],
-			account: account,
-		});
+		try {
+			const response = await msalInstance.acquireTokenSilent({
+				scopes: [config.ACCESS_SCOPE],
+				account: account,
+			});
 
-		return response.accessToken;
+			return response.accessToken;
+		} catch (e) {
+			if (e instanceof InteractionRequiredAuthError) {
+				const response = await msalInstance.acquireTokenPopup({
+					scopes: [config.ACCESS_SCOPE],
+					account: account,
+				});
+				return response.accessToken;
+			}
+			throw e;
+		}
 	} else {
 		throw new Error("No accounts found");
 	}
 };
 
-const token = await getAccessToken();
-
 const api = axios.create({
 	baseURL: config.API_HOST,
-	headers: {
-		"Authorization": `Bearer ${token}`
-	}
 });
+
+api.interceptors.request.use(
+	async (config) => {
+		const token = await getAccessToken();
+		config.headers.Authorization = `Bearer ${token}`;
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
 
 export {
 	api
