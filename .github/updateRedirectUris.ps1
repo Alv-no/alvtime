7ed7427be7f26e862eb7e48d8b916aa5569374f6
sondre-alv <sondre@alv.no>
2026-01-27 11:02:41 +0100

    dvgfsfs

--- .github/updateRedirectUris.ps1
+++ .github/updateRedirectUris.ps1
@@ -1,1 +1,17 @@
-az account show
+$clientId = '148ac485-a123-479c-8ed3-bfaa56619e04'
+$newUri = "https://ap.no"
+
+
+$existingRedirectUrisList = az ad app show --id $clientId --query spa.redirectUris 
+
+$cleanedRedirectUriList = $existingRedirectUrisList | Out-String | ForEach-Object { $_ -replace '\s', '' } | ForEach-Object { $_ -replace '"', '' } | ForEach-Object { $_ -replace '\[|\]', '' } | ForEach-Object { $_.Split(',') }
+
+
+$urisToSet = $cleanedRedirectUriList + $newUri
+
+$uriListString = $urisToSet -join "','"
+
+$setArgument = "spa={'redirectUris': ['" + $uriListString + "']}"
+
+
+az ad app update --id $clientId --set $setArgument
