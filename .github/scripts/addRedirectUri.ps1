f04468d5fab645d8a16a1259d9a27a3331baafed
sondre-alv <sondre@alv.no>
2026-01-28 16:09:23 +0100

    sadfgdaf

--- .github/scripts/addRedirectUri.ps1
+++ .github/scripts/addRedirectUri.ps1
@@ -7,6 +7,10 @@ $existingRedirectUrisList = az ad app show --id $appRegistrationClientId --query
 
 $cleanedRedirectUriList = $existingRedirectUrisList | Out-String | ForEach-Object { $_ -replace '\s', '' } | ForEach-Object { $_ -replace '"', '' } | ForEach-Object { $_ -replace '\[|\]', '' } | ForEach-Object { $_.Split(',') }
 
+if ($cleanedRedirectUriList -contains $uriToAdd) {
+    Write-Host "The URI '$uriToAdd' already exists in the redirect URIs. No action taken."
+    exit 0
+}
 
 $urisToSet = $cleanedRedirectUriList + $uriToAdd
 
