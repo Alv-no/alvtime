0fc68ddd90880028648917484bc3265aef12e659
sondre-alv <sondre@alv.no>
2026-01-30 13:48:43 +0100

    Small frontend change to trigger workflow

--- packages/frontend-v3/src/components/NavigationBar.vue
+++ packages/frontend-v3/src/components/NavigationBar.vue
@@ -110,8 +110,8 @@ const close = () => {
 		width: 1200px;
 		padding: 0 20px;
 		display: flex;
-		align-items: center;
 		justify-content: space-between;
+		align-items: center;
 
 		a {
 			text-decoration: none;
