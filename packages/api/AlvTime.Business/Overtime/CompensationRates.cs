9a6278d7e56b0e6c4b5bb47792910a92d79f45e0
Pål Bøckmann <pal@alv.no>
2026-05-18 16:01:01 +0200

    Add CopmensationRates constants

---
+++ packages/api/AlvTime.Business/Overtime/CompensationRates.cs
@@ -0,0 +1,9 @@
+﻿namespace AlvTime.Business.Overtime;
+
+public static class CompensationRates
+{
+    public const decimal Volunteer = 0.5M;
+    public const decimal Internal = 1.0M;
+    public const decimal Billable = 1.5M;
+    public const decimal Imposed = 2.0M;
+}
