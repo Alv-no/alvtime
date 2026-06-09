a5700333a384f83397f6feb7a6a820785ba333ea
Pål Bøckmann <pal.bockmann@pm.me>
2026-06-09 14:32:32 +0200

    Deleted old frontend

--- packages/frontend/jest_global_mocks.js
+++
@@ -1,6 +0,0 @@
-jest.mock("@/config.ts");
-jest.mock("@/services/auth", () => ({
-  addCallback: () => Promise.resolve(),
-  getAccount: () => {},
-  getAccountAsync: () => Promise.resolve(null),
-}));
