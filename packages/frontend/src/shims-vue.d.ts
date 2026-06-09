97f4777fdc0b485f1160edaebffa97fde625a748
Pål Bøckmann <41721443+palbock@users.noreply.github.com>
2026-06-09 14:49:48 +0200

    Deleted old frontend (#1194)

--- packages/frontend/src/shims-vue.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.vue" {
-  import Vue from "vue";
-  export default Vue;
-}
-declare module "vue-material/dist/components";
-declare module "vue-awesome-swiper";
-declare module "vue-mq";
-declare module "@/services/lifecycle.es5.js";
-declare module "@/services/createMsalApp.js";
