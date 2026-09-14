6d5e28ac1ac281d1a47d6011f2236682f6adc8f1
Pål Bøckmann <41721443+palbock@users.noreply.github.com>
2026-09-14 14:58:03 +0200

    Reading environment from server rather than client (#1222)

---
+++ packages/adminpanel/Server/Controllers/ConfigController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alvtime.Adminpanel.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ConfigController(IConfiguration configuration) : ControllerBase
+{
+    [HttpGet("apiURL")]
+    public ActionResult<string> GetApiURL()
+    {
+        return configuration["ApiSettings:BaseUrl"]!;
+    }
+}
