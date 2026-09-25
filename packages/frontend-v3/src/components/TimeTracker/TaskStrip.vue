ffb1f95c5cbf9194d2f8d4811dfa45687b3de938
Boye Svensson <boyevnsvensson@gmail.com>
2026-09-25 14:03:52 +0200

    Add hover effect to task strip (#1224)
    The TaskStrip component now provides visual feedback on hover.
    This includes a background color change and an expanding border,
    enhancing interactivity and user experience.

--- packages/frontend-v3/src/components/TimeTracker/TaskStrip.vue
+++ packages/frontend-v3/src/components/TimeTracker/TaskStrip.vue
@@ -31,11 +31,16 @@ const compensationRateInPercentage = computed(() => {
 <style lang="scss" scoped>
 .task-strip {
 	display: flex;
+	position: relative;
 	justify-content: space-between;
-	align-items: center;
-	border-radius: 5px;
+	align-items: baseline;
 	font-size: 1.2rem;
 	flex-wrap: wrap;
+	text-box: trim-both cap alphabetic;
+	
+	.task-description {
+		text-box: trim-both cap alphabetic;
+	}
 
 	@media screen and (max-width: 768px) {
 		.task-description {
@@ -46,6 +51,26 @@ const compensationRateInPercentage = computed(() => {
 		.task-week {
 			width: 100%;
 			margin-top: 0.5rem;
+		}
+	}
+
+	&::after {
+		content: '';
+		position: absolute;
+		inset: -5px -10px; // -vertikal -horisontal (topp/bunn -10px, venstre/høyre -15px)
+		border-style: solid;
+		border-width: 5px 10px; // topp/bunn 10px, venstre/høyre 15px
+		border-color: transparent;
+		box-sizing: border-box;
+		pointer-events: none; // så pseudo-elementet ikke stjeler hover/klikk
+		border-radius: 10px;
+	}
+
+	&:hover {
+		background-color: rgb(230, 192, 141);
+
+		&::after {
+		border-color: rgb(230, 192, 141);
 		}
 	}
 }
